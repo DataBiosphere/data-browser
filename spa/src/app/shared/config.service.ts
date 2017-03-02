93765d8e28a20066ecdbfaccf8f18c771e0da787
noopdog <dave@clevercanary.com>
2017-03-02 18:52:26 -0500

    changing to point to UCSC prod

--- spa/src/app/shared/config.service.ts
+++ spa/src/app/shared/config.service.ts
@@ -6,7 +6,7 @@ export type ApiSource = "UCSC_STAGE" | "UCSC" | "ICGC";
 @Injectable()
 export class ConfigService {
 
-    private source: ApiSource = "UCSC_STAGE";
+    private source: ApiSource = "UCSC";
 
     constructor() { }
 
@@ -33,7 +33,7 @@ export class ConfigService {
     hasSortOrder() {
 
         return false;
-        //return this.source === "UCSC_STAGE" || this.source === "UCSC";
+        // return this.source === "UCSC_STAGE" || this.source === "UCSC";
     }
 
     getTestSortFacets(): FileFacetMetadata[] {
