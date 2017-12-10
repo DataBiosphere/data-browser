3df92ebc16bdaaac5f9184ee60515a5a5a120b38
noopdog <dave@clevercanary.com>
2017-12-09 21:20:16 -0500

    Deleted unused keywords component and tests

--- spa/src/app/keywords/keywords.module.ts
+++ spa/src/app/keywords/keywords.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { KeywordsComponent } from "./keywords.component";
 import { KeywordsService } from "./shared/keywords.service";
 import { KeywordsDAO } from "./shared/keywords.dao";
 
@@ -8,7 +7,6 @@ import { KeywordsDAO } from "./shared/keywords.dao";
     imports: [
         CommonModule
     ],
-    declarations: [KeywordsComponent],
     providers: [KeywordsService, KeywordsDAO]
 })
 export class KeywordsModule {
