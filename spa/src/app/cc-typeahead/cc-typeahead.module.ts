b8a5155a743fb7577ebdead49e6279edb5413f2e
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2017-12-12 09:53:05 +1000

    Mat styles for facet search updated.

--- spa/src/app/cc-typeahead/cc-typeahead.module.ts
+++ spa/src/app/cc-typeahead/cc-typeahead.module.ts
@@ -2,7 +2,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
-import { MatInputModule } from "@angular/material";
+import { MatFormFieldModule, MatInputModule } from "@angular/material";
 
 // App dependencies
 import { CcTypeaheadComponent } from "./cc-typeahead.component";
@@ -10,6 +10,7 @@ import { CcTypeaheadComponent } from "./cc-typeahead.component";
 @NgModule({
     imports: [
         CommonModule,
+        MatFormFieldModule,
         MatInputModule,
         ReactiveFormsModule
     ],
