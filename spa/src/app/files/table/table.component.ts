95b5d88557c5e3416686ed776ee1c966f5308507
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2017-12-13 16:18:23 +1000

    Double and full width cards.

--- spa/src/app/files/table/table.component.ts
+++ spa/src/app/files/table/table.component.ts
@@ -16,7 +16,6 @@ import { PaginationModel } from "./pagination.model";
 export class TableComponent implements OnInit {
 
     displayedColumns = ["program", "project", "donor_id", "submitter", "specimen_type", "sample", "workflow", "filename", "file_id", "size"];
-    dataSource = new ExampleDataSource();
     tableElementDataSource: TableElementDataSource;
     pagination$: Observable<PaginationModel>;
 
@@ -69,163 +68,6 @@ export interface Element {
     size: number;
 }
 
-const data: Element[] = [
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-PASFGG-T-D",
-        specimen_type: "Primary tumour - solid tissue",
-        sample: "TARGET-30-PASFGG-T-D",
-        workflow: "spinnaker:1.0.0",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 2.75
-    },
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 3.75
-    },
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 4.75
-    },
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 5.75
-    },
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 6.75
-    },
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 7.75
-    },
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 8.75
-    },
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 3.75
-    },
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 3.75
-    }, {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 3.75
-    }, {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 3.75
-    },
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 3.75
-    },
-    {
-        program: "PROTECT_NBL",
-        project: "TARGET",
-        donor_id: "protect_01",
-        submitter: "TARGET-30-T-D",
-        specimen_type: "Primary tumour",
-        sample: "TARGET-30",
-        workflow: "spinnaker:1.0.4",
-        filename: "5a00caf9-f202-4bcb-9fca-7d255ce171dc_1.fastq.gz",
-        file_id: "07fc1bfd-650a-5c64-9e5e-2aef10c3a73f",
-        size: 3.75
-    }
-];
-
 class TableElementDataSource extends DataSource<any> {
 
     element$: Observable<Element[]>;
@@ -256,16 +98,6 @@ class TableElementDataSource extends DataSource<any> {
 
     connect(): Observable<Element[]> {
         return this.element$;
-    }
-
-    disconnect() {
-    }
-}
-
-class ExampleDataSource extends DataSource<any> {
-    /** Connect function called by the table to retrieve one stream containing the data to render. */
-    connect(): Observable<Element[]> {
-        return Observable.of(data);
     }
 
     disconnect() {
