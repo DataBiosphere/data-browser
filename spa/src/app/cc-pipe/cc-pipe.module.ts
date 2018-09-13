0e335c8d8f3770a0e8248a098fd311dc873adbf3
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2018-09-13 10:00:46 +1000

    Count size

--- spa/src/app/cc-pipe/cc-pipe.module.ts
+++ spa/src/app/cc-pipe/cc-pipe.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
+import { CountSizePipe } from "./count-size/count-size.pipe";
 import { FileSizePipe } from "./file-size/file-size.pipe";
 import { LocaleStringPipe } from "./locale-string/locale-string.pipe";
 import { CamelToSpacePipe } from "./camel-to-space/camel-to-space.pipe";
 
 const pipes = [
+    CountSizePipe,
     FileSizePipe,
     LocaleStringPipe,
     CamelToSpacePipe
---
+++ spa/src/app/cc-pipe/count-size/count-size.pipe.ts
@@ -0,0 +1,37 @@
+/**
+ * Pipe for formatting count sizes.
+ */
+
+// Core dependencies
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: "countSize"
+})
+export class CountSizePipe implements PipeTransform {
+
+    transform(value: number): string {
+
+        const countSizes = ["k", "M", "G", "T", "P", "E"];
+
+        // Determine count size display value and unit
+        let val = value || 0;
+        let sigFig = 0;
+        while (val >= 1000) {
+            val = val / 1000;
+            sigFig += 1;
+            console.log(val, sigFig);
+        }
+
+        // No format of count size - tens, hundreds
+        if ( sigFig === 0 ) {
+            return `${val}`;
+        }
+
+        // Format of count size to "n.0k"
+        // Round value to precision
+        let precision = 1;
+        const roundedValue = val.toFixed(precision);
+        return `${roundedValue}${countSizes[(sigFig) - 1]}`;
+    }
+}
