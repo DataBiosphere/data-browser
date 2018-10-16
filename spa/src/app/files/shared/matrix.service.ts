5dc65211eb02ac7df539b3a77ff172e5215d72f3
MillenniumFalconMechanic <mim@clevercanary.com>
2018-10-17 08:12:51 +1000

    Reverted dummy code.

--- spa/src/app/files/shared/matrix.service.ts
+++ spa/src/app/files/shared/matrix.service.ts
@@ -35,18 +35,18 @@ export class MatrixService {
      */
     public getMatrixStatus(requestId: string): Observable<MatrixResponse> {
 
-        // return this.matrixDAO.getMatrixStatus(requestId);
-        const num = Math.floor(Math.random() * Math.floor(2));
-        console.log(num);
+        return this.matrixDAO.getMatrixStatus(requestId);
 
-        return Observable.of({
-            eta: "10 seconds",
-            key: "123",
-            links: ["http://www.google.com"],
-            message: "message goes here",
-            requestId: "request id",
-            status: num === 1 ? MatrixStatus.COMPLETE : MatrixStatus.IN_PROGRESS
-        });
+        // const num = Math.floor(Math.random() * Math.floor(2));
+        // console.log(num);
+        // return Observable.of({
+        //     eta: "10 seconds",
+        //     key: "123",
+        //     links: ["http://www.google.com"],
+        //     message: "message goes here",
+        //     requestId: "request id",
+        //     status: num === 1 ? MatrixStatus.COMPLETE : MatrixStatus.IN_PROGRESS
+        // });
     }
 
     /**
@@ -60,16 +60,16 @@ export class MatrixService {
 
         // Build up the manifest URL
         const manifestUrl = this.filesService.buildManifestUrl(selectedFacets);
-        // return this.matrixDAO.requestMatrix(manifestUrl, matrixFormat);
+        return this.matrixDAO.requestMatrix(manifestUrl, matrixFormat);
 
-        return Observable.of({
-            eta: "10 seconds",
-            key: "123",
-            links: ["http://www.google.com"],
-            message: "message goes here",
-            requestId: "request id",
-            status: MatrixStatus.IN_PROGRESS
-        });
+        // return Observable.of({
+        //     eta: "10 seconds",
+        //     key: "123",
+        //     links: ["http://www.google.com"],
+        //     message: "message goes here",
+        //     requestId: "request id",
+        //     status: MatrixStatus.IN_PROGRESS
+        // });
     }
 
     /**
