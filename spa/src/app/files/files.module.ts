8ea41b42c79b516108d48f5f38c009ce76c3f668
noopdog <dave@clevercanary.com>
2018-10-25 12:23:42 +1000

    fix merge errors

--- spa/src/app/files/files.module.ts
+++ spa/src/app/files/files.module.ts
@@ -12,7 +12,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
-import { HttpClientModule } from "@angular/common/http";
+
 import {
     MatAutocompleteModule,
     MatButtonModule,
