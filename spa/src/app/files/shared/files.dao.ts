7d755363a0d932d370d92b1dbcd9685d8aad5eb9
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2018-10-30 18:01:03 +1000

    WIP

--- spa/src/app/files/shared/files.dao.ts
+++ spa/src/app/files/shared/files.dao.ts
@@ -291,7 +291,7 @@ export class FilesDAO extends CCBaseDAO {
                 responseTerms = responseFileFacet.terms.map((responseTerm) => {
 
                     if (responseTerm.term == null) {
-                        responseTerm.term = "Unspecified";
+                        responseTerm.term = null;
                     }
 
                     let oldTerm: Term;
