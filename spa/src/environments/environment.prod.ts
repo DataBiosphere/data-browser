e3cb9f9ee6fe2597739510c9507a0127039ac8ae
noopdog <dave@clevercanary.com>
2018-11-14 20:03:44 +1000

    update prod environment urls

--- spa/src/environments/environment.prod.ts
+++ spa/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    dataURL: "https://service.staging.explore.data.humancellatlas.org",
-    matrixURL: "https://matrix.staging.data.humancellatlas.org/v0/matrix",
-    portalURL: "https://pre-prod.data.humancellatlas.org"
+    dataURL: "https://service.explore.data.humancellatlas.org",
+    matrixURL: "https://matrix.data.humancellatlas.org/v0/matrix",
+    portalURL: "https://prod.data.humancellatlas.org"
 };
