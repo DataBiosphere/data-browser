dfad9cc426ef9ebe6a6a14d1668fcf603c633b65
noopdog <dave@clevercanary.com>
2018-12-03 21:42:52 +1000

    insert Google Tag Manager tags into index.html for dev

---
+++ insert-gtm-snippet.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+echo $CI_ENVIRONMENT_NAME
+
+gtmHead=$(cat "./gtm/"$CI_ENVIRONMENT_NAME".head.txt")
+gtmBody=$(cat "./gtm/"$CI_ENVIRONMENT_NAME".body.txt")
+
+echo $gtmHead
+echo $gtmBody
+
+
+replaceHead=$"s/<!-- REPLACE-ME: GOOGLE TAG MANAGER HEAD -->/""$gtmHead"/g
+replaceBody=$"s/<!-- REPLACE-ME: GOOGLE TAG MANAGER BODY -->/""$gtmBody"/g
+
+sed -i .bak "$replaceHead" spa/src/index.html
+sed -i .bak "$replaceBody" spa/src/index.html
+
+
