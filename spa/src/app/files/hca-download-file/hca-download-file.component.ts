b606d29fd8f547b4065b916f3e0d5ab145e72756
MillenniumFalconMechanic <mim@clevercanary.com>
2019-01-11 12:59:17 +1000

    Updated file download to use native browser download.

--- spa/src/app/files/hca-download-file/hca-download-file.component.ts
+++ spa/src/app/files/hca-download-file/hca-download-file.component.ts
@@ -7,24 +7,24 @@
  */
 
 // Core dependencies
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from "@angular/core";
+import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from "@angular/core";
 import "rxjs/add/observable/of";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { Observable } from "rxjs/Observable";
 import { Subject } from "rxjs/Subject";
 
 // App dependencies
+import { Element } from "../hca-table-files/hca-table-files.component";
 import { DownloadService } from "../shared/download.service";
+import { FileDownloadResponse } from "../shared/download-response.model";
 import { FileDownloadStatus } from "../shared/file-download-status.model";
-import {Element} from "../hca-table-files/hca-table-files.component";
 
 @Component({
     selector: "hca-download-file",
     templateUrl: "./hca-download-file.component.html",
     styleUrls: ["./hca-download-file.component.scss"]
 })
-export class HCADownloadFileComponent implements OnDestroy, OnInit{
-
+export class HCADownloadFileComponent implements OnDestroy, OnInit {
 
     // Locals
     private ngDestroy$ = new Subject();
@@ -32,22 +32,21 @@ export class HCADownloadFileComponent implements OnDestroy, OnInit{
     public fileUrl: string;
 
     // Template variables
-    downloadStatus$ = new BehaviorSubject<FileDownloadStatus>(FileDownloadStatus.NOT_STARTED);
+    downloadResponse$ = new BehaviorSubject<FileDownloadResponse>({
+        status: FileDownloadStatus.NOT_STARTED
+    });
 
     // Inputs
     @Input() file: Element;
     @Output() fileUrlGenerated = new EventEmitter<string>();
 
+    // View child/ren
+    @ViewChild("download") downloadEl: ElementRef;
+
     /**
      * @param {DownloadService} downloadService
      */
     constructor(private downloadService: DownloadService) {
-    }
-
-    ngOnInit(): void {
-       this.fileName = this.file.fileName;
-       this.fileUrl = this.file.url;
-
     }
 
     /**
@@ -57,68 +56,164 @@ export class HCADownloadFileComponent implements OnDestroy, OnInit{
     /**
      * Returns true if download has completed.
      *
-     * @param {FileDownloadStatus} status
+     * @param {FileDownloadResponse} response
      * @returns {boolean}
      */
-    public isDownloadComplete(status: FileDownloadStatus): boolean {
+    public isDownloadComplete(response: FileDownloadResponse): boolean {
 
-        return status === FileDownloadStatus.COMPLETE;
+        return this.downloadService.isFileDownloadRequestCompleted(response);
     }
 
     /**
      * Returns true if download has failed.
      *
-     * @param {FileDownloadStatus} status
+     * @param {FileDownloadResponse} response
      * @returns {boolean}
      */
-    public isDownloadFailed(status: FileDownloadStatus): boolean {
+    public isDownloadFailed(response: FileDownloadResponse): boolean {
 
-        return status === FileDownloadStatus.FAILED;
+        return this.downloadService.isFileDownloadRequestFailed(response);
+    }
+
+    /**
+     * Returns true if browser has started actual download of file.
+     *
+     * @param {FileDownloadResponse} response
+     * @returns {boolean}
+     */
+    public isDownloading(response: FileDownloadResponse): boolean {
+
+        return this.downloadService.isFileDownloading(response);
+    }
+
+    /**
+     * Returns true if file download request has been initiated. That is, user has requested file download but response
+     * from server has not yet been received.
+     *
+     * @param {FileDownloadResponse} response
+     * @returns {boolean}
+     */
+    public isDownloadInitiated(response: FileDownloadResponse): boolean {
+
+        return this.downloadService.isFileDownloadInitiated(response);
     }
 
     /**
      * Returns true if download has not yet been initiated.
      *
-     * @param {FileDownloadStatus} status
+     * @param {FileDownloadResponse} response
      * @returns {boolean}
      */
-    public isDownloadNotStarted(status: FileDownloadStatus): boolean {
+    public isDownloadNotStarted(response: FileDownloadResponse): boolean {
 
-        return status === FileDownloadStatus.NOT_STARTED;
+        return this.downloadService.isFileDownloadRequestNotStarted(response);
     }
 
     /**
-     * Returns true if download has started.
+     * Returns true if file download has been requested.
      *
-     * @param {FileDownloadStatus} status
+     * @param {FileDownloadResponse} response
      * @returns {boolean}
      */
-    public isDownloadInProgress(status: FileDownloadStatus): boolean {
+    public isDownloadInProgress(response: FileDownloadResponse): boolean {
 
-        return status === FileDownloadStatus.IN_PROGRESS;
+        return this.downloadService.isFileDownloadRequestInProgress(response);
     }
 
     /**
      * Return observable of download status, for binding to in template.
      *
-     * @returns {Observable<FileDownloadStatus>}
+     * @returns {Observable<FileDownloadResponse>}
      */
-    public observeFileDownloadStatus(): Observable<FileDownloadStatus> {
+    public observeFileDownloadStatus(): Observable<FileDownloadResponse> {
 
-        return this.downloadStatus$.asObservable();
+        return this.downloadResponse$.asObservable();
     }
 
     /**
-     * Send request to download file.
+     * Initiate file download request.
      */
     public onRequestFile() {
 
-        // Request file download
-        this.downloadService.downloadFile(this.fileName, this.fileUrl)
-            .takeUntil(this.ngDestroy$)
-            .subscribe((status) => {
-                this.downloadStatus$.next(status);
+        // Update file download status to indicate user has initiated file download.
+        this.downloadResponse$.next({
+            status: FileDownloadStatus.INITIATED
+        });
+
+        // Set up poller to keep checking on status of file download.
+        this.downloadResponse$
+            .subscribe((response: FileDownloadResponse) => {
+                if ( this.isDownloadInProgress(response) ) {
+                    // this.updateFileDownloadStatus(response);
+                }
+                else if ( this.isDownloadComplete(response) ) {
+                    this.downloadFile(response.location);
+                }
             });
+
+        // Request file download
+        this.downloadService.requestFileDownload(this.fileUrl)
+            .subscribe((response) => {
+                this.downloadResponse$.next(response);
+            });
+    }
+
+    /**
+     * True from the moment user clicks the file download button through to when the browser initiates the actual
+     * download of the file.
+     */
+    public showSpinner(response: FileDownloadResponse): boolean {
+
+        return this.isDownloadInitiated(response) ||
+            this.isDownloadInProgress(response) ||
+            // Include COMPLETE here as there is a delay between when the "complete" status is received
+            // from the server and when the browser initiates the actual download.
+            this.isDownloadComplete(response);
+    }
+
+    /**
+     * File download request is completed - initiate file download.
+     *
+     * @param {string} url
+     */
+    private downloadFile(url: string) {
+
+        const el = this.downloadEl.nativeElement;
+        el.href = url;
+        el.click();
+
+        // Update file download status to indicate browser is starting actual download, delaying by a second
+        // to cover browser time to initiate (and indicate start of) download.
+        this.emitAfterInterval(() => {
+            this.downloadResponse$.next({
+                status: FileDownloadStatus.DOWNLOADING
+            });
+        }, 1000);
+    }
+
+    /**
+     * Execute function after specified delay.
+     *
+     * @param {Function} fn
+     * @param {number} delay (milliseconds)
+     */
+    private emitAfterInterval(fn, delay: number) {
+
+        Observable
+            .interval(delay)
+            .take(1)
+            .subscribe(fn);
+    }
+
+    /**
+     * Request update of download file request status, after a delay according to the previously returned "retry after"
+     * value.
+     *
+     * @param {FileDownloadResponse} response
+     */
+    private updateFileDownloadStatus(response: FileDownloadResponse) {
+
+        this.emitAfterInterval(response => this.downloadResponse$.next(response), response.retryAfter * 1000);
     }
 
     /**
@@ -132,5 +227,17 @@ export class HCADownloadFileComponent implements OnDestroy, OnInit{
 
         this.ngDestroy$.next(true);
         this.ngDestroy$.complete();
+    }
+
+    /**
+     * Set up initial component state.
+     */
+    public ngOnInit(): void {
+
+        this.fileName = this.file.fileName;
+        this.fileUrl = this.file.url;
+
+        // Kill download subject on destroy of component
+        this.downloadResponse$.takeUntil(this.ngDestroy$);
     }
 }
