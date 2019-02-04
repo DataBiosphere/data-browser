4b20fb638d7b55dfa86ee7430cec8bedc3f0926a
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2019-02-04 20:28:12 +1000

    Placeholder for no table data results

--- spa/src/app/files/hca-table-pagination/hca-table-pagination.component.ts
+++ spa/src/app/files/hca-table-pagination/hca-table-pagination.component.ts
@@ -35,9 +35,8 @@ export class HCATablePaginationComponent implements OnInit {
     // Template variables
     pagination$: Observable<PaginationModel>;
 
-    // Locals
-
     // Inputs
+    @Input() isData: any[];
 
     /**
      * @param {Store<AppState>} store
