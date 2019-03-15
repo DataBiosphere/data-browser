b2804d0f3952c3275f9203fa691e686c4d70b3ce
MillenniumFalconMechanic <mim@clevercanary.com>
2019-03-15 10:45:55 -0700

    Added error status code and message state. Added error message to error page.

--- spa/src/app/_ngrx/app.reducer.ts
+++ spa/src/app/_ngrx/app.reducer.ts
@@ -9,6 +9,7 @@
 import * as authReducer from "../auth/_ngrx/auth.reducer";
 import * as configReducer from "../config/_ngrx/config.reducer";
 import * as fileReducer from "../files/_ngrx/file.reducer";
+import * as httpReducer from "../http/_ngrx/http.reducer";
 import * as keywordReducer from "../keywords/_ngrx/keyword.reducer";
 import * as systemReducer from "../system/_ngrx/system.reducer";
 
@@ -16,6 +17,7 @@ export const AppReducers = {
     ...fileReducer.reducer,
     auth: authReducer.reducer,
     config: configReducer.reducer,
+    http: httpReducer.reducer,
     keywords: keywordReducer.reducer,
     ...systemReducer.reducer
 };
