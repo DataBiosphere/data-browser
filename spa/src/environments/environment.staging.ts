2d2fdb7a5267a705c7d0535809202ae4ce441a18
MillenniumFalconMechanic <mim@clevercanary.com>
2019-03-29 10:43:09 -0700

    Updated project metadata URL per environment.

--- spa/src/environments/environment.staging.ts
+++ spa/src/environments/environment.staging.ts
@@ -3,5 +3,5 @@ export const environment = {
     dataURL: "https://service.staging.explore.data.humancellatlas.org",
     matrixURL: "https://matrix.staging.data.humancellatlas.org/v0/matrix",
     portalURL: "https://staging.data.humancellatlas.org",
-    projectMetaURL: "http://url.data.humancellatlas.org"
+    projectMetaURL: "http://staging.url.data.humancellatlas.org"
 };
--- spa/src/environments/environment.ux-dev.ts
+++ spa/src/environments/environment.ux-dev.ts
@@ -3,5 +3,5 @@ export const environment = {
     dataURL: "https://service.dev.explore.data.humancellatlas.org",
     matrixURL: "https://matrix.integration.data.humancellatlas.org/v0/matrix",
     portalURL: "https://ux-dev.data.humancellatlas.org",
-    projectMetaURL: "http://url.data.humancellatlas.org"
+    projectMetaURL: "http://dev.dev.url.data.humancellatlas.org"
 };
