bd1c32957243f4c33c122b6f688b71e25350f7a1
MillenniumFalconMechanic <mim@clevercanary.com>
2019-03-29 10:43:09 -0700

    Updated project metadata URL per environment.

--- spa/src/environments/environment.integration.ts
+++ spa/src/environments/environment.integration.ts
@@ -3,5 +3,5 @@ export const environment = {
     dataURL: "https://service.integration.explore.data.humancellatlas.org",
     matrixURL: "https://matrix.integration.data.humancellatlas.org/v0/matrix",
     portalURL: "https://integration.data.humancellatlas.org",
-    projectMetaURL: "http://url.data.humancellatlas.org"
+    projectMetaURL: "http://integration.url.data.humancellatlas.org"
 };
