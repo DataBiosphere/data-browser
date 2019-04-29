28c426a03db67a94d59b86b2ec783e5dd1e2b10a
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2019-04-30 08:58:07 +1000

    Project table spacing and rebase

--- spa/src/app/files/shared/file-facet-group.model.ts
+++ spa/src/app/files/shared/file-facet-group.model.ts
@@ -1,7 +1,7 @@
-/*
+/**
  * Human Cell Atlas
  * https://www.humancellatlas.org/
- * 
+ *
  * Model of facet/entity names grouped by a "group name", for display in file facet menus. For example, the facet names
  * "organ", "organPart" and "selectedCellType" are grouped under "organ".
  */
