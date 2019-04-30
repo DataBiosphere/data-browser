59d3a8ad2357b5d1a7f7c3dbb70bfd1e8361589e
Mim Hastie <mim@clevercanary.com>
2019-04-30 11:44:10 -0700

    Added chips to manifest and terra modals. (#579)

--- spa/src/app/files/hca-export-to-terra-modal/hca-export-to-terra-modal.state.ts
+++ spa/src/app/files/hca-export-to-terra-modal/hca-export-to-terra-modal.state.ts
@@ -8,11 +8,13 @@
 // App dependencies
 import { FileSummary } from "../file-summary/file-summary";
 import { ExportToTerraStatus } from "../shared/export-to-terra-status.model";
+import { SearchTerm } from "../search/search-term.model";
 
 export interface HCAExportToTerraModalState {
 
     exportToTerraStatus: ExportToTerraStatus;
     exportToTerraUrl: string;
     manifestDownloadFileSummary: FileSummary;
+    selectedSearchTerms: SearchTerm[];
     selectedSearchTermNames: string[];
 }
