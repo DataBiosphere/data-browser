e00f5c60a0370433bd0f63768e3ab91fe463640d
Mim Hastie <mim@clevercanary.com>
2019-05-01 15:32:36 -0700

    Added cut off of term facet list height to indicate more. (#588)

--- spa/src/app/files/hca-facet-term-list/hca-facet-term-list.component.ts
+++ spa/src/app/files/hca-facet-term-list/hca-facet-term-list.component.ts
@@ -7,7 +7,7 @@
  */
 
 // Core dependencies
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, QueryList, Output, Renderer2, ViewChildren } from "@angular/core";
 
 // App dependencies
 import { CountSizePipe } from "../../cc-pipe/count-size/count-size.pipe";
@@ -20,7 +20,7 @@ import { Term } from "../shared/term.model";
     templateUrl: "hca-facet-term-list.component.html",
     styleUrls: ["hca-facet-term-list.component.scss"]
 })
-export class HCAFacetTermListComponent {
+export class HCAFacetTermListComponent implements AfterViewInit {
 
     // Inputs
     @Input() fileFacetName: string;
@@ -30,6 +30,14 @@ export class HCAFacetTermListComponent {
     // Outputs
     @Output() facetTermSelected = new EventEmitter<FileFacetTermSelectedEvent>();
 
+    // View child/ren
+    @ViewChildren("facetTerm") termElRefs: QueryList<ElementRef>;
+
+    /**
+     * @param {ElementRef} el
+     */
+    constructor(private el: ElementRef, private renderer: Renderer2) {}
+    
     /**
      * Return the inline style configuration for the chart legend, for the specified term.
      *
@@ -95,5 +103,89 @@ export class HCAFacetTermListComponent {
     public trackByFn(index: number, term: Term): string {
 
         return term.name;
+    }
+
+    /**
+     * Calculate the heights of each term list, "cutting" the last visible term in any list that extend beyond the 
+     * bottom of the menu, to indicate more.
+     */
+    ngAfterViewInit() {
+
+        // Grab the list element and its parent container
+        const listEl = this.el.nativeElement;
+        const parentEl = listEl.parentElement;
+        const listOffset = listEl.offsetTop - parentEl.offsetTop;
+    
+        // Determine the heights of the list element and the list's parent
+        const nativeElHeight = listEl.getBoundingClientRect().height;
+        const parentElHeight = parentEl.getBoundingClientRect().height;
+
+        // We don't need to proceed if the list fits into its parent container
+        if ( nativeElHeight + listOffset <= parentElHeight) {
+            return;
+        }
+
+        // Calculate the new height of the list parent, depending on how many terms are visible
+        let updatedParentHeight = 0;
+        let heightTally = listOffset;
+        let previousHeightTally = heightTally;
+        let lastVisibleTermElRef: ElementRef;
+        for ( let termElRef of this.termElRefs.toArray() ) {
+
+            // Determine the height of the terms up to and including this term
+            const termHeight = termElRef.nativeElement.getBoundingClientRect().height;
+            const heightIncludingTerm = heightTally + termHeight;
+            
+            // Move on to the next term if this term is fully visible, updating total height of terms up to and including
+            // this term and keeping a record of the previous tally (in case we need to cut the previous term)
+            if ( heightIncludingTerm < parentElHeight ) {
+
+                previousHeightTally = heightTally;
+                heightTally = heightIncludingTerm;
+                lastVisibleTermElRef = termElRef;
+            }
+            // Otherwise we have found the first term that is not fully visible. Determine if we should cut this
+            // term's or the previous term's visibility, to indicate there are more terms
+            else {
+
+                const visibleSpace = parentElHeight - heightTally;
+                const percentVisible = visibleSpace / termHeight;
+
+                // If less than half of the term is visible, roll back the height to cut the last fully visible term
+                if ( percentVisible < 0.5 && lastVisibleTermElRef ) {
+                    updatedParentHeight = 
+                        previousHeightTally + this.calculateCheckBoxCutOffHeight(lastVisibleTermElRef.nativeElement);
+                }
+                // Otherwise more than half the current term is visible, cut current term height back to 50%
+                else {
+                    updatedParentHeight = heightTally + this.calculateCheckBoxCutOffHeight(termElRef.nativeElement);
+                }
+
+                break;
+            }
+        }
+
+        // Update parent to new height total
+        this.renderer.setStyle(parentEl, "height", `${updatedParentHeight}px`);
+    }
+
+    /**
+     * Return the height of where to cut off the checkbox element, that is a child of the specified term list item.
+     * 
+     * @param {HTMLElement} termEl
+     * @returns {number}
+     */
+    private calculateCheckBoxCutOffHeight(termEl: HTMLElement): number {
+
+        // Find the term's checkbox
+        const checkboxes = termEl.getElementsByClassName("chart-legend-bar");
+        if ( !checkboxes.length ) {
+            return 0;
+        }
+
+        // Add the offset of the checkbox to half the checkbox height; we only want to show half of the checkbox 
+        const checkbox = checkboxes[0] as HTMLElement;
+        const height = (checkbox.offsetTop - termEl.offsetTop) + Math.floor(checkbox.getBoundingClientRect().height / 2);
+        return height;
     }
 }
