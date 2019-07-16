1b40df34c92400f5100517e728c6f63a27e7b6e4
noopdog <dave@clevercanary.com>
2019-07-16 12:13:16 -0700

    update metadata url to use https and new cloudfront

--- spa/src/environments/environment.staging.ts
+++ spa/src/environments/environment.staging.ts
@@ -3,6 +3,6 @@ export const environment = {
     dataURL: "https://service.staging.explore.data.humancellatlas.org",
     matrixURL: "https://matrix.staging.data.humancellatlas.org/v0/matrix",
     portalURL: "https://staging.data.humancellatlas.org",
-    projectMetaURL: "http://staging.url.data.humancellatlas.org",
+    projectMetaURL: "https://staging.data.humancellatlas.org",
     deployment: "staging"
 };
