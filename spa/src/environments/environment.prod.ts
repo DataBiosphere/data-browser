cee8594251adf9cb471750f21bf7b791daa11d3b
noopdog <dave@clevercanary.com>
2019-08-01 14:05:55 -0700

    update url to drop prod dot

--- spa/src/environments/environment.prod.ts
+++ spa/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     dataURL: "https://service.explore.data.humancellatlas.org",
     matrixURL: "https://matrix.data.humancellatlas.org/v0/matrix",
-    portalURL: "https://prod.data.humancellatlas.org",
-    projectMetaURL: "https://prod.data.humancellatlas.org",
+    portalURL: "https://data.humancellatlas.org",
+    projectMetaURL: "https://data.humancellatlas.org",
     deployment: "prod"
 };
