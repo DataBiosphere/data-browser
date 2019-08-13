44e16120a7f0992d538a9395e1a0ed2f860be76d
Mim Hastie <mim@clevercanary.com>
2019-08-13 14:43:45 -0700

    Fixed jiggle of matrix download text. (#794)

---
+++ spa/src/app/files/hca-get-data/hca-get-data.state.ts
@@ -0,0 +1,20 @@
+/**
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * State backing get data component.
+ */
+
+// App dependencies
+import { Config } from "../../config/config.model";
+import EntitySpec from "../shared/entity-spec";
+import { FileFacet } from "../shared/file-facet.model";
+
+export interface HCAGetDataState {
+
+    config: Config;
+    fileFacets: FileFacet[];
+    matrixSupported: boolean;
+    matrixSupportedLoaded: boolean;
+    selectedEntity: EntitySpec;
+}
