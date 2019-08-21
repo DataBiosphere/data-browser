c3ae8d774f19b811786e9606872e43b1d456d4ab
Fran McDade <frano-m@users.noreply.github.com>
2019-08-22 03:08:03 +1000

    Table column alignments for right aligned content. Resolves #807. (#810)

--- spa/src/app/files/hca-content-ellipsis/hca-content-ellipsis.component.ts
+++ spa/src/app/files/hca-content-ellipsis/hca-content-ellipsis.component.ts
@@ -20,12 +20,25 @@ import { HCAEllipsisTextComponent } from "./hca-ellipsis-text.component";
 
 export class HCAContentEllipsisComponent {
 
-    // Inputs
-    @Input() tooltipContent: string;
-    
-    // // View child/ren
+    // View child/ren
     @ViewChild(HCAEllipsisTextComponent, {read: ElementRef}) textElementRef: ElementRef;
     @ViewChild(HCATooltipComponent, {read: ElementRef}) tooltipElementRef: ElementRef;
+
+    /**
+     * Returns the text content of the element of interest.
+     *
+     * @returns {string}
+     */
+    public getTooltipContent(): string {
+
+        // Return empty string if there is no text child
+        if ( !this.textElementRef ) {
+            return "";
+        }
+
+        // Grab the content of the text child
+        return this.textElementRef.nativeElement.textContent;
+    }
 
     /**
      * Returns true (disable tooltip) if the width of the element of interest is less than its parent container.
@@ -42,9 +55,9 @@ export class HCAContentEllipsisComponent {
 
         // Grab the width of the text to be displayed
         const contentWidth = this.textElementRef.nativeElement.getBoundingClientRect().width;
-        
-        // We can use the tooltip to determine the width available 
-        const containerWidth = this.tooltipElementRef.nativeElement.getBoundingClientRect().width; 
+
+        // We can use the tooltip to determine the width available
+        const containerWidth = this.tooltipElementRef.nativeElement.getBoundingClientRect().width;
 
         // The tooltip is only enabled if the width of the ellipsis content is wider than the container width
         return contentWidth <= containerWidth;
