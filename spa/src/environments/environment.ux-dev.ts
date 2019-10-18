2bb1c82a683129b142fbc50f0a47f663e4beac98
noopdog <dave@clevercanary.com>
2019-10-18 14:32:34 -0700

    point ux-dev to prod

--- spa/src/environments/environment.ux-dev.ts
+++ spa/src/environments/environment.ux-dev.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    dataURL: "https://service.dev.explore.data.humancellatlas.org",
-    dcpHealthCheckUrl: "https://status.dev.data.humancellatlas.org/service/dcp-health-check-dev",
-    matrixURL: "https://matrix.integration.data.humancellatlas.org/v0/matrix",
+    dataURL: "https://service.explore.data.humancellatlas.org",
+    dcpHealthCheckUrl: "https://status.data.humancellatlas.org/service/dcp-health-check-dev",
+    matrixURL: "https://matrix.data.humancellatlas.org/v0/matrix",
     portalURL: "https://ux-dev.data.humancellatlas.org",
-    projectMetaURL: "https://dev.data.humancellatlas.org",
+    projectMetaURL: "https://data.humancellatlas.org",
     deployment: "ux-dev"
 };
