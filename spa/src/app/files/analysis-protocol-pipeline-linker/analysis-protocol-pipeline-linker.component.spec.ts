c706c55818e1dc3c1f50405dc19883e072a831d2
MillenniumFalconMechanic <mim@clevercanary.com>
2019-11-06 10:38:43 -0800

    Updated naming test conventions. Resolves #950.

--- spa/src/app/files/analysis-protocol-pipeline-linker/analysis-protocol-pipeline-linker.component.spec.ts
+++ spa/src/app/files/analysis-protocol-pipeline-linker/analysis-protocol-pipeline-linker.component.spec.ts
@@ -79,7 +79,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm get pipeline link returns corresponding data portal optimus link when analysis protocol includes "optimus".
      */
-    it(`should get pipeline link returns corresponding data portal optimus link when analysis protocol includes "optimus"`, () => {
+    it(`returns data portal optimus link when analysis protocol includes "optimus"`, () => {
 
         // Confirm link is for data portal optimus
         const pipelineLink = component.getPipelineLink(TEST_VALUE_OPTIMUS);
@@ -89,7 +89,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm get pipeline link returns corresponding data portal smartseq2 link when analysis protocol includes "smartseq2".
      */
-    it(`should get pipeline link returns corresponding data portal smartseq2 link when analysis protocol includes "smartseq2"`, () => {
+    it(`returns data portal smartseq2 link when analysis protocol includes "smartseq2"`, () => {
 
         // Confirm link is for data portal smartseq2
         const pipelineLink = component.getPipelineLink(TEST_VALUE_SMARTSEQ2);
@@ -99,7 +99,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm get pipeline link returns the root base path when there is no corresponding pipeline.
      */
-    it("should get pipeline link returns the root base path when there is no corresponding pipeline", () => {
+    it("returns root base path when there is no corresponding pipeline (error flow)", () => {
 
         // Confirm no link is returned
         const pipelineLink = component.getPipelineLink(TEST_VALUE_CELL_RANGER);
@@ -109,7 +109,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm is analysis protocol linked returns true when analysis protocol includes "optimus".
      */
-    it(`should is analysis protocol linked returns true when analysis protocol includes "optimus"`, () => {
+    it(`recognizes "optimus" as a workflow with a corresponding data portal pipeline link`, () => {
 
         // Confirm true is returned
         const analysisProtocolLinked = component.isAnalysisProtocolLinked(TEST_VALUE_OPTIMUS);
@@ -119,7 +119,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm is analysis protocol linked returns true when analysis protocol includes "smartseq2".
      */
-    it(`should is analysis protocol linked returns true when analysis protocol includes "smartseq2"`, () => {
+    it(`recognizes "smartseq2" as a workflow with a corresponding data portal pipeline link`, () => {
 
         // Confirm true is returned
         const analysisProtocolLinked = component.isAnalysisProtocolLinked(TEST_VALUE_SMARTSEQ2);
@@ -129,7 +129,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm is analysis protocol linked returns false when analysis protocol neither includes "smartseq2" nor "optimus".
      */
-    it(`should is analysis protocol linked returns true when analysis protocol neither includes "smartseq2" nor "optimus"`, () => {
+    it(`recognizes that a worflow with neither "smartseq2" nor "optimus" has no data portal pipeline link`, () => {
 
         // Confirm true is returned
         const analysisProtocolLinked = component.isAnalysisProtocolLinked(TEST_VALUE_CELL_RANGER);
@@ -139,7 +139,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm list analysis protocols returns a concatenated string array when multiple workflow values.
      */
-    it("should list analysis protocols returns a concatenated string array when multiple workflow values", () => {
+    it("splits multiple workflow values into a string array with multiple values", () => {
 
         component.workflow = INPUT_VALUE_WORKFLOW_MULTIPLE_MIXED_VALUES;
         fixture.detectChanges();
@@ -152,7 +152,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm list analysis protocols returns a string array when single workflow values.
      */
-    it("should list analysis protocols returns a string array when single workflow values", () => {
+    it("converts a single workflow value into a string array with one value", () => {
 
         component.workflow = INPUT_VALUE_WORKFLOW_SINGLE_OPTIMUS_VALUE;
         fixture.detectChanges();
@@ -165,7 +165,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm list analysis protocols returns an empty array when empty workflow values.
      */
-    it("should list analysis protocols returns an empty array when empty workflow values", () => {
+    it("returns an empty array when there is no workflow", () => {
 
         component.workflow = "";
         fixture.detectChanges();
@@ -176,52 +176,46 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     });
 
     /**
-     * Confirm list analysis protocols returns a string array when single workflow values.
-     */
-    it("should list analysis protocols returns a string array when single workflow values", () => {
-
-        component.workflow = INPUT_VALUE_WORKFLOW_SINGLE_OPTIMUS_VALUE;
-        fixture.detectChanges();
-
-        // Confirm get analysis protocols returns a string array
-        const analysisProtocols = component.listAnalysisProtocols();
-        expect(analysisProtocols).toEqual([TEST_VALUE_OPTIMUS]);
-    });
-
-    /**
      * Confirm find analysis protocol key returns "optimus" when analysis protocol includes "optimus".
      */
-    it(`should find analysis protocol key return "optimus" when analysis protocol includes "optimus"`, () => {
+    it(`returns protocol key "optimus" when analysis protocol includes "optimus"`, () => {
 
         // Confirm key returned is "optimus"
-        const analysisProtocolKey = component["findAnalysisProtocolKey"](LOCAL_VALUE_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY, TEST_VALUE_OPTIMUS);
-        expect(analysisProtocolKey).toEqual(Object.keys(LOCAL_VALUE_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY)[INDEX_OF_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY_OPTIMUS]);
+        const analysisProtocolKey =
+            component["findAnalysisProtocolKey"](LOCAL_VALUE_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY, TEST_VALUE_OPTIMUS);
+        const key =
+            Object.keys(LOCAL_VALUE_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY)[INDEX_OF_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY_OPTIMUS];
+        expect(analysisProtocolKey).toEqual(key);
     });
 
     /**
      * Confirm find analysis protocol key returns "smartseq2" when analysis protocol includes "smartseq2".
      */
-    it(`should find analysis protocol key return "smartseq2" when analysis protocol includes "smartseq2"`, () => {
+    it(`returns protocol key "smartseq2" when analysis protocol includes "smartseq2"`, () => {
 
         // Confirm key returned is "smartseq2"
-        const analysisProtocolKey = component["findAnalysisProtocolKey"](LOCAL_VALUE_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY, TEST_VALUE_SMARTSEQ2);
-        expect(analysisProtocolKey).toEqual(Object.keys(LOCAL_VALUE_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY)[INDEX_OF_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY_SMARTSEQ2]);
+        const analysisProtocolKey =
+            component["findAnalysisProtocolKey"](LOCAL_VALUE_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY, TEST_VALUE_SMARTSEQ2);
+        const key =
+            Object.keys(LOCAL_VALUE_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY)[INDEX_OF_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY_SMARTSEQ2];
+        expect(analysisProtocolKey).toEqual(key);
     });
 
     /**
      * Confirm find analysis protocol key returns undefined when analysis protocol is neither includes "smartseq2" nor "optimus".
      */
-    it(`should find analysis protocol key return undefined when analysis protocol is neither includes "smartseq2" nor "optimus"`, () => {
+    it(`returns undefined when analysis protocol is neither includes "smartseq2" nor "optimus"`, () => {
 
         // Confirm key returned is undefined
-        const analysisProtocolKey = component["findAnalysisProtocolKey"](LOCAL_VALUE_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY, TEST_VALUE_CELL_RANGER);
+        const analysisProtocolKey =
+            component["findAnalysisProtocolKey"](LOCAL_VALUE_PIPELINE_LINKS_BY_ANALYSIS_PROTOCOL_KEY, TEST_VALUE_CELL_RANGER);
         expect(analysisProtocolKey).toBeUndefined();
     });
 
     /**
      * Confirm all unlinked analysis protocols are displayed, when multiple unlinked workflow.
      */
-    it("should display all unlinked analysis protocols when multiple unlinked workflow", () => {
+    it("displays plain text for a list of workflows that have no corresponding pipelines", () => {
 
         // Set up initial component state
         component.workflow = INPUT_VALUE_WORKFLOW_MULTIPLE_UNLINKED_VALUES;
@@ -235,7 +229,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm all linked analysis protocols are displayed, when multiple linked workflow.
      */
-    it("should display all linked analysis protocols when multiple linked workflow", () => {
+    it("displays links for a list of workflows that all have corresponding pipelines", () => {
 
         // Set up initial component state
         component.workflow = INPUT_VALUE_WORKFLOW_MULTIPLE_LINKED_VALUES;
@@ -249,7 +243,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm all analysis protocols are displayed, when multiple mixed workflow.
      */
-    it("should display all analysis protocols when multiple mixed workflow", () => {
+    it("displays links and plain text for analysis protocols when multiple mixed workflow", () => {
 
         // Set up initial component state
         component.workflow = INPUT_VALUE_WORKFLOW_MULTIPLE_MIXED_VALUES;
@@ -264,7 +258,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm no analysis protocols are displayed, when workflow is empty.
      */
-    it("should display no analysis protocols when workflow is empty", () => {
+    it("displays no analysis protocols when workflow is empty", () => {
 
         // Set up initial component state
         component.workflow = "";
@@ -279,7 +273,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm data portal optimus pipeline link is added to href attribute when workflow includes "optimus".
      */
-    it(`should data portal optimus pipeline link is added to href attribute when workflow includes "optimus"`, () => {
+    it(`links to optimus pipeline when workflow includes "optimus"`, () => {
 
         // Set up initial component state
         component.workflow = INPUT_VALUE_WORKFLOW_SINGLE_OPTIMUS_VALUE;
@@ -295,7 +289,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm data portal smartseq2 pipeline link is added to href attribute when workflow includes "smartseq2".
      */
-    it(`should data portal smartseq2 pipeline link is added to href attribute when workflow includes "smartseq2"`, () => {
+    it(`links to smartseq2 pipeline when workflow includes "smartseq2"`, () => {
 
         // Set up initial component state
         component.workflow = INPUT_VALUE_WORKFLOW_SINGLE_SMARTSEQ2_VALUE;
@@ -311,7 +305,7 @@ describe("AnalysisProtocolPipelineLinkerComponent", () => {
     /**
      * Confirm comma is not displayed last when multiple workflow.
      */
-    it("should not display comma last when multiple workflow", () => {
+    it("correctly delimits list of workflows", () => {
 
         // Set up initial component state
         component.workflow = INPUT_VALUE_WORKFLOW_MULTIPLE_MIXED_VALUES;
