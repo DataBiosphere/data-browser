a1d2f7d1b12136cf38bcdc5ef8974d785153338f
noopdog <dave@clevercanary.com>
2019-12-04 11:47:14 -0800

    fix ux-dev config for prod

--- spa/src/environments/environment.ux-dev.ts
+++ spa/src/environments/environment.ux-dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     dataURL: "https://service.explore.data.humancellatlas.org",
-    dcpHealthCheckUrl: "https://status.data.humancellatlas.org/service/dcp-health-check-dev",
+    dcpHealthCheckUrl: "https://status.data.humancellatlas.org/service/dcp-health-check-prod",
     matrixURL: "https://matrix.data.humancellatlas.org/v0/matrix",
     portalURL: "https://ux-dev.data.humancellatlas.org",
     projectMetaURL: "https://data.humancellatlas.org",
