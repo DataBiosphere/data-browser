9a401e546d97b9eef56d3851fc3baedcb64d6dc6
noopdog <dave@clevercanary.com>
2020-03-23 13:43:29 -0700

    update file name spec

---
+++ mar-2020-release-sync.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+
+aws s3 sync --acl public-read ../hca-2020-mar-release s3://release-files.data.humancellatlas.org --delete  --exclude "*.DS_Store" --dryrun --profile hca-admin
