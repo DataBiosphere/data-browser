90d1e4f84689f1ac8dde610d95276578d12142cc
noopdog <dave@clevercanary.com>
2020-03-23 22:13:53 -0700

    release sync script

--- mar-2020-release-sync.sh
+++ mar-2020-release-sync.sh
@@ -2,4 +2,6 @@
 set -e
 
 
-aws s3 sync --acl public-read ../hca-2020-mar-release s3://release-files.data.humancellatlas.org --delete  --exclude "*.DS_Store" --dryrun --profile hca-admin
+#aws s3 sync --acl public-read ../hca-2020-mar-release s3://release-files.data.humancellatlas.org --delete  --exclude "*.DS_Store" --dryrun --profile hca-admin
+
+aws s3 sync --acl public-read ../hca-2020-mar-release s3://release-files.data.humancellatlas.org --delete  --exclude "*.DS_Store"  --profile hca-admin
