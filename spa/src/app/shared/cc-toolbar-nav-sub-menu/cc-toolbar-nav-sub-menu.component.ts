6e44fd834d555ece2680dd28b4d5fa33a6e13ab3
Fran McDade <frano-m@users.noreply.github.com>
2020-04-14 06:36:56 +1000

    Add 2nd level nav to 'March 2020 Release' in top nav so we can show both 'Datasets' and 'Documentation' options. Resolves #1131. (#1142)

---
+++ spa/src/app/shared/cc-toolbar-nav-sub-menu/cc-toolbar-nav-sub-menu.component.ts
@@ -0,0 +1,15 @@
+// Dependencies
+import { Component } from "@angular/core";
+
+/**
+ * Component for displaying nav sub menu, used in conjunction with cc-toolbar-nav.
+ */
+
+@Component({
+    selector: "cc-toolbar-nav-sub-menu",
+    templateUrl: "cc-toolbar-nav-sub-menu.component.html",
+    styleUrls: ["cc-toolbar-nav-sub-menu.component.scss"],
+})
+
+export class CCToolbarNavSubMenuComponent {
+}
