29646e4a3ab05db3733452ce5d47b084bb35b2f1
Mim Hastie <mim@clevercanary.com>
2020-07-31 14:08:56 -0700

    Corrected termination of Terra polling. Resolves #1278. (#1287)

--- spa/src/app/files/_ngrx/terra/terra.effects.ts
+++ spa/src/app/files/_ngrx/terra/terra.effects.ts
@@ -10,16 +10,18 @@ import { Injectable } from "@angular/core";
 import { Actions, Effect, ofType } from "@ngrx/effects";
 import { Action, select, Store } from "@ngrx/store";
 import { Observable, of } from "rxjs";
-import { concatMap, map, switchMap, take, withLatestFrom } from "rxjs/operators";
+import { concatMap, filter, map, switchMap, take, withLatestFrom } from "rxjs/operators";
 
 // App dependencies
 import { AppState } from "../../../_ngrx/app.state";
 import { ExportToTerraInProgressAction } from "./export-to-terra-in-progress.action";
 import { ExportToTerraRequestAction } from "./export-to-terra-request.action";
 import { ExportToTerraSuccessAction } from "./export-to-terra-success.action";
-import { selectSelectedSearchTerms } from "../search/search.selectors";
-import { TerraService } from "../../shared/terra.service";
 import { selectFileFormatsFileFacet } from "../facet/facet.selectors";
+import { selectSelectedSearchTerms } from "../search/search.selectors";
+import { ExportToTerraStatus } from "../../shared/export-to-terra-status.model";
+import { TerraService } from "../../shared/terra.service";
+import { selectExportToTerra } from "./terra.selectors";
 
 @Injectable()
 export class TerraEffects {
@@ -46,10 +48,24 @@ export class TerraEffects {
                     this.store.pipe(select(selectFileFormatsFileFacet), take(1))
                 )
             )),
-            switchMap(([action, searchTerms, fileFormatsFileFacet]) => 
-                this.terraService.exportToTerra(searchTerms, fileFormatsFileFacet)),
-            map(response => this.terraService.isExportToTerraRequestInProgress(response.status) ?
-                new ExportToTerraInProgressAction(response) :
-                new ExportToTerraSuccessAction(response))
+            switchMap(([action, searchTerms, fileFormatsFileFacet]) => {
+
+                // Set up the kill switch for the polling of the Tera export. We'll use the value of the response
+                // object in the store, and only stop polling if the response state returns to NOT_STARTED (which occurs
+                // if user navigates away from export component).
+                const killSwitch$ = this.store.pipe(
+                    select(selectExportToTerra),
+                    map(terraState => terraState.exportToTerraStatus === ExportToTerraStatus.NOT_STARTED),
+                    filter(cleared => cleared), // Only allow value to emit if file manifest response has been cleared from the store
+                    take(1)
+                );
+                
+                return this.terraService.exportToTerra(searchTerms, fileFormatsFileFacet, killSwitch$);
+            }),
+            map(response => {
+                return this.terraService.isExportToTerraRequestInProgress(response.status) ?
+                    new ExportToTerraInProgressAction(response) :
+                    new ExportToTerraSuccessAction(response)
+            })
         );
 }
