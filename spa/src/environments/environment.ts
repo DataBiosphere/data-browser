eab5e0fb69dac060639d55ba23490b55abcc9e6d
Fran McDade <franmcdade@Frans-MBP.hub>
2020-08-21 13:56:08 +1000

    Point local environment DP links to staging DP instance. Resolves #1277.

--- spa/src/environments/environment.ts
+++ spa/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     dataURL: "https://service.dev.explore.data.humancellatlas.org",
     dcpHealthCheckUrl: "https://status.dev.data.humancellatlas.org/service/dcp-health-check-dev",
     matrixURL: "https://matrix.staging.data.humancellatlas.org/v0/matrix",
-    portalURL: "https://dev.data.humancellatlas.org",
+    portalURL: "https://staging.data.humancellatlas.org",
     projectMetaURL: "https://dev.data.humancellatlas.org",
     deployment: "local",
     version: "1.0"
