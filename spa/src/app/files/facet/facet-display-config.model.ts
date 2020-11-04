8881e965d3c8582e761cbc8ff4c9d3ef751cd844
Mim Hastie <mim@clevercanary.com>
2020-11-04 14:02:57 -0800

    Added development stage facet. Resolves #1364. (#1377)

---
+++ spa/src/app/files/facet/facet-display-config.model.ts
@@ -0,0 +1,12 @@
+/**
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * Model of facet config, including spec for environments where facet is applicable.
+ */
+
+export interface FacetDisplayConfig {
+
+    facetName: string,
+    versions: string[];
+}
