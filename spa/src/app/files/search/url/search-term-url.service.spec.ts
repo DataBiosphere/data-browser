9696649abc7b87472a0799524ddb8aab7f076fce
MillenniumFalconMechanic <mim@clevercanary.com>
2021-02-17 14:12:37 -0800

    Added filter validation. Resolves #1444.

---
+++ spa/src/app/files/search/url/search-term-url.service.spec.ts
@@ -0,0 +1,116 @@
+/*
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * Spec for testing search term URL service.
+ */
+
+// Core dependencies
+import { async } from "@angular/core/testing";
+
+// App dependencies
+import { SearchTermUrlService } from "./search-term-url.service";
+import { QueryStringSearchTerm } from "./query-string-search-term.model";
+
+describe("SearchTermUrlService", () => {
+
+    let searchTermUrlService;
+
+    beforeEach(async(() => {
+
+        searchTermUrlService = new SearchTermUrlService();
+    }));
+
+    describe("parseQueryStringSearchTerms", () => {
+
+        /**
+         * Returns empty array when no filter is specified.
+         */
+        it("returns empty array when no filter is specified", () => {
+
+            const searchTerms = searchTermUrlService.parseQueryStringSearchTerms({});
+            expect(searchTerms).toEqual([]);
+        });
+
+        /**
+         * Parses valid filter
+         */
+        it("parses valid filter", () => {
+
+            const params = {
+                filter: `[{"facetName":"foo","terms":["bar","baz"]}]`
+            };
+            const searchTerms = searchTermUrlService.parseQueryStringSearchTerms(params);
+            const expected = [new QueryStringSearchTerm("foo", ["bar", "baz"])];
+            expect(searchTerms).toEqual(expected);
+        });
+
+        /**
+         * Parses age range valid filter
+         */
+        it("parses valid age range filter", () => {
+
+            const params = {
+                filter: `[{"facetName":"foo","terms":[{"ageMin":0,"ageMax":10,"ageUnit":"year"}]}]`
+            };
+            const searchTerms = searchTermUrlService.parseQueryStringSearchTerms(params);
+            const expected = [new QueryStringSearchTerm("foo", [{
+                ageMin: 0,
+                ageMax: 10,
+                ageUnit: "year"
+            }])];
+            expect(searchTerms).toEqual(expected);
+        });
+
+        describe("parse errors", () => {
+
+            const parseErrorParams = [
+                {filter: "foo"},
+                {filter: "{}"},
+                {filter: "[]"}
+            ];
+
+            /**
+             * Throws parse errors on filters that are invalid JSON.
+             */
+            parseErrorParams.forEach(params => {
+
+                it(`throws error for invalid filter: ${params.filter}`, () => {
+
+                    const expectedMessage = 
+                        `${searchTermUrlService["ERROR_TEXT_PARSE"]} "${searchTermUrlService["PARAM_FILTER"]}": ${params.filter}`;
+                    expect(() => searchTermUrlService.parseQueryStringSearchTerms(params)).toThrowError(expectedMessage);
+                });
+
+            });
+        });
+
+        describe("syntax errors", () => {
+
+            const syntaxErrorParams = [
+                {filter: `[{"terms":""}]`},
+                {filter: `[{"facetName":"foo"}]`},
+                {filter: `[{"facetName":"foo","terms":"bar"}]`},
+                {filter: `[{"facetName":"foo", "terms": [{}]}]`},
+                {filter: `[{"facetName":"foo","terms":[{"ageMin":0}]}]`}
+            ];
+
+            /**
+             * Throws parse errors on filters that are invalid JSON.
+             */
+            syntaxErrorParams.forEach(params => {
+
+                it(`throws error for invalid filter: ${params.filter}`, () => {
+
+                    const expectedMessage =
+                        `${searchTermUrlService["ERROR_TEXT_SYNTAX"]} "${searchTermUrlService["PARAM_FILTER"]}": ${params.filter}`;
+                    expect(() => searchTermUrlService.parseQueryStringSearchTerms(params)).toThrowError(expectedMessage);
+                });
+
+            });
+        });
+    });
+
+    xdescribe("stringifySearchTerms", () => {
+    });
+});
--- spa/src/app/files/search/url/search-term-url.service.ts
+++ spa/src/app/files/search/url/search-term-url.service.ts
@@ -11,22 +11,24 @@
 import { Injectable } from "@angular/core";
 
 // App dependencies
-import { Facet } from "../../facet/facet.model";
-import { FileFacetName } from "../../facet/file-facet/file-facet-name.model";
 import { SearchTerm } from "../search-term.model";
 import { Params } from "@angular/router";
-import { GenusSpecies } from "../../shared/genus-species.model";
 import { SearchTermUrl } from "./search-term-url.model";
 import { QueryStringSearchTerm } from "./query-string-search-term.model";
 
 @Injectable()
 export class SearchTermUrlService {
 
+    private PARAM_FILTER = "filter";
+    private ERROR_TEXT_PARSE = "Unable to parse query string parameter";
+    private ERROR_TEXT_SYNTAX = "Invalid query string parameter";
+
     /**
      * Parse filter query string parameter, if present, and convert string to query string search terms. Called when
      * initializing app state from the query string.
      *
      * @param {Params} params
+     * @throws {Error}
      */
     public parseQueryStringSearchTerms(params: Params): QueryStringSearchTerm[] {
 
@@ -35,20 +37,30 @@ export class SearchTermUrlService {
         }
 
         // We have a filter, let's extract it.
+        const filterParam = params[this.PARAM_FILTER];
         let filter;
-        const filterParam = params["filter"];
         try {
-            filter = JSON.parse(filterParam);
+            filter = JSON.parse(filterParam); // throws SyntaxError
         }
-        catch (error) {
-            console.log(error);
+        catch(e) {
+            throw Error(this.buildParseErrorMessage(this.PARAM_FILTER, filterParam));
         }
 
         // Confirm filter was parsed correctly.
-        if ( !filter || !filter.length || !filter[0].facetName ) {
-            return [];
+        if ( !filter || !filter.length ) {
+            throw Error(this.buildParseErrorMessage(this.PARAM_FILTER, filterParam));
         }
-
+        
+        // Confirm each filter value has both facetName and terms values
+        if ( !this.isFilterSyntaxValid(filter) ) {
+            throw Error(this.buildSyntaxErrorMessage(this.PARAM_FILTER, filterParam));
+        }
+        
+        // Confirm terms values are valid
+        if ( !this.isTermsSyntaxValid(filter) ) {
+            throw Error(this.buildSyntaxErrorMessage(this.PARAM_FILTER, filterParam));
+        }
+        
         // Convert filter into query string search terms.
         return filter.map((selectedFacet) => {
             return new QueryStringSearchTerm(selectedFacet[SearchTermUrl.FACET_NAME], selectedFacet[SearchTermUrl.VALUE]);
@@ -86,6 +98,30 @@ export class SearchTermUrlService {
     }
 
     /**
+     * Build parse error message.
+     * 
+     * @param {string} paramName
+     * @param {string} filterParam
+     * @returns {string}
+     */
+    private buildParseErrorMessage(paramName: string, filterParam: string): string {
+
+        return `${this.ERROR_TEXT_PARSE} "${paramName}": ${filterParam}`;
+    }
+
+    /**
+     * Build syntax error message.
+     *
+     * @param {string} paramName
+     * @param {string} filterParam
+     * @returns {string}
+     */
+    private buildSyntaxErrorMessage(paramName: string, filterParam: string): string {
+
+        return `${this.ERROR_TEXT_SYNTAX} "${paramName}": ${filterParam}`;
+    }
+
+    /**
      * Returns true if a filter state is encoded in the query params.
      *
      * @param {Params} params
@@ -94,6 +130,48 @@ export class SearchTermUrlService {
     private isFilterParamSpecified(params: Params): boolean {
 
         return !!params["filter"];
+    }
+
+    /**
+     * Returns true if each value in filter param is valid. That is, each value has both facetName and terms values.
+     * 
+     * @param {any} filter
+     */
+    private isFilterSyntaxValid(filter): boolean {
+
+        return filter.every(f => {
+            return f.facetName && f.terms;
+        });
+    }
+
+    /**
+     * Returns true if terms values are valid. terms must be a string array.
+     *
+     * @param {any} filter
+     */
+    private isTermsSyntaxValid(filter): boolean {
+
+        return filter.every(f => {
+            const terms = f.terms;
+            if ( !Array.isArray(terms) ) {
+                return false;
+            }
+            return terms.every(t => {
+                return typeof t === "string" || this.isTermAgeRangeSyntaxValid(t);
+            })
+        });
+    }
+
+    /**
+     * Returns true if terms value is a valid age range value.
+     *
+     * @param {any} term
+     */
+    private isTermAgeRangeSyntaxValid(term): boolean {
+        
+        return (term.ageMax || term.ageMax === 0) &&
+            (term.ageMin || term.ageMin === 0) &&
+            term.ageUnit;
     }
 }
 
