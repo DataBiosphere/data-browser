de7c1675d431618c1647de5b1a08b879239a17d4
David Rogers <dave@clevercanary.com>
2021-03-08 10:11:17 -0800

    lungmap-dev gitlab build setup (#1561)
    Co-authored-by: Dave Rogers <dave@Daves-MacBook-Pro.local>

---
+++ spa/src/environments/environment.lungmap-dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    atlas: "lungmap",
+    production: true,
+    dataURL: "https://service.dev.singlecell.gi.ucsc.edu",
+    dcpHealthCheckUrl: "https://status.data.humancellatlas.org/service/dcp-health-check-prod",
+    matrixURL: "https://matrix.data.humancellatlas.org/v0/matrix",
+    portalURL: "https://dev.singlecell.gi.ucsc.edu",
+    projectMetaURL: "https://dev.singlecell.gi.ucsc.edu",
+    terraExportURL: "https://bvdp-saturn-dev.appspot.com",
+    deployment: "lungmap-dev",
+    version: "2.0",
+    zendeskURL: ""
+};
