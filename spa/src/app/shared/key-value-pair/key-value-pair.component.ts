e2aadbd88dc5e46d65f31a964a9f8b62cc4c8732
MillenniumFalconMechanic <mim@clevercanary.com>
2021-03-25 17:11:29 -0700

    Added tracking of accessions. Resolves #1360.

--- spa/src/app/shared/key-value-pair/key-value-pair.component.ts
+++ spa/src/app/shared/key-value-pair/key-value-pair.component.ts
@@ -6,7 +6,7 @@
  */
 
 // Core dependencies
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 // App dependencies
 import { KeyValuePair } from "./key-value-pair.model";
@@ -18,9 +18,10 @@ import { KeyValuePair } from "./key-value-pair.model";
 })
 export class KeyValuePairComponent {
 
-    // Inputs
+    // Inputs/Outputs
     @Input() keyValuePairs: KeyValuePair[];
     @Input() title: string;
+    @Output() valueClicked = new EventEmitter<KeyValuePair>();
 
     /**
      * Returns display text when there are no key value pairs.
@@ -43,5 +44,15 @@ export class KeyValuePairComponent {
     public isMultiValue(value: string | string[]): boolean {
 
         return Array.isArray(value);
+    }
+
+    /**
+     * Let parent components know value has been clicked.
+     * 
+     * @param {KeyValuePair} pair
+     */
+    public onValueClicked(pair: KeyValuePair) {
+
+        this.valueClicked.emit(pair);
     }
 }
