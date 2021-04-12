df7a2bb1372a3dd09de1d6a3361cde7a8a9bc0ec
MillenniumFalconMechanic <mim@clevercanary.com>
2021-04-12 09:19:53 -0700

    Updated default catalog to dcp4. Resolves #1664.

--- spa/src/environments/environment.dcp2.ts
+++ spa/src/environments/environment.dcp2.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     atlas: "hca",
     dataURL: "https://service.azul.data.humancellatlas.org",
-    defaultCatalog: "dcp3",
+    defaultCatalog: "dcp4",
     portalURL: "https://data.humancellatlas.org",
     projectMetaURL: "https://data.humancellatlas.org",
     terraExportURL: "https://app.terra.bio",
