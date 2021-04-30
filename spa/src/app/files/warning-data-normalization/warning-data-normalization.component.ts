d3e9d04140f029d38d542a69ca720a0eeeff81c5
MillenniumFalconMechanic <mim@clevercanary.com>
2021-04-30 12:33:16 -0700

    Added data normalization warning to bulk download. Resolves #1677.

---
+++ spa/src/app/files/warning-data-normalization/warning-data-normalization.component.ts
@@ -0,0 +1,16 @@
+/*
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * Component for displaying data normalization warning on download pages.
+ */
+
+// Core dependencies
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "warning-data-normalization",
+    templateUrl: "./warning-data-normalization.component.html",
+    styleUrls: ["./warning-data-normalization.component.scss"]
+})
+export class WarningDataNormalizationComponent {}
