5a7cb3cdeeebe5786a8458c2d13f4dafd78fe63b
MillenniumFalconMechanic <mim@clevercanary.com>
2021-05-07 15:31:22 -0700

    Updated warning text and location. Resolves #1677.

--- spa/src/app/files/warning-data-normalization/warning-data-normalization.component.ts
+++ spa/src/app/files/warning-data-normalization/warning-data-normalization.component.ts
@@ -8,9 +8,23 @@
 // Core dependencies
 import { Component } from "@angular/core";
 
+// App dependencies
+import { ConfigService } from "../../config/config.service";
+
 @Component({
     selector: "warning-data-normalization",
     templateUrl: "./warning-data-normalization.component.html",
     styleUrls: ["./warning-data-normalization.component.scss"]
 })
-export class WarningDataNormalizationComponent {}
+export class WarningDataNormalizationComponent {
+
+    // Template variables
+    portalUrl;
+
+    /**
+     * @param configService
+     */
+    constructor(private configService: ConfigService) {
+        this.portalUrl = configService.getPortalUrl();
+    }
+}
