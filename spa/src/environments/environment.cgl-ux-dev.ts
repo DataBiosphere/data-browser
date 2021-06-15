f08e52d979bb154a56f86c16fceb684a4082bfa7
Dave Rogers <dave@Daves-MacBook-Pro.local>
2021-06-15 10:33:18 -0700

    added ux dev env

---
+++ spa/src/environments/environment.cgl-ux-dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    atlas: "hca",
+    dataURL: "https://service.dev.singlecell.gi.ucsc.edu",
+    defaultCatalog: "dcp2",
+    browserURL: "https://ux-dev.singlecell.gi.ucsc.edu",
+    portalURL: "https://ux-dev.singlecell.gi.ucsc.edu",
+    projectMetaURL: "https://ux-dev.singlecell.gi.ucsc.edu",
+    terraExportURL: "https://bvdp-saturn-dev.appspot.com",
+    title: "HCA Data Browser",
+    deployment: "cgl-ux-dev",
+    zendeskURL: "https://support.terra.bio"
+};
