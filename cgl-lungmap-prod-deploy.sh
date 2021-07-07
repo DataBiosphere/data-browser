61dea158cb0c0c24361143d7e9f4bdd573c0dc17
Dave Rogers <dave@Daves-MacBook-Pro.local>
2021-07-07 10:38:24 -0700

    add GA to lungmap prod

--- cgl-lungmap-prod-deploy.sh
+++ cgl-lungmap-prod-deploy.sh
@@ -1,14 +1,43 @@
 #!/usr/bin/env bash
+set -e
 
-export GATSBY_GTM_ID=GTM-M2J5NTJ
-export GATSBY_GTM_AUTH=CWB1d2Aoi1q3GOmQMSGMsZA
-export GATSBY_ENV_NAME=env-150
+export GTM_ID=GTM-M2J5NTJ
+export GTM_AUTH=CWB1d2Aoi1q3GOmQMSGMsZA
+export GTM_ENV=env-150
+
+gtmHead=$(cat "./gtm/gtm.head.txt")
+gtmBody=$(cat "./gtm/gtm.body.txt")
+
+echo "GTM_ID is: "
+echo $GTM_ID
+
+gtmHead=$(echo $gtmHead | sed s/GTM_ID/"$GTM_ID"/g)
+gtmHead=$(echo $gtmHead | sed s/GTM_AUTH/"$GTM_AUTH"/g)
+gtmHead=$(echo $gtmHead | sed s/GTM_ENV/"$GTM_ENV"/g)
+
+gtmBody=$(echo $gtmBody | sed s/GTM_ID/"$GTM_ID"/g)
+gtmBody=$(echo $gtmBody | sed s/GTM_AUTH/"$GTM_AUTH"/g)
+gtmBody=$(echo $gtmBody | sed s/GTM_ENV/"$GTM_ENV"/g)
+
+echo $gtmHead
+echo $gtmBody
+
+
+replaceHead=$"s/<!-- REPLACE-ME: GOOGLE TAG MANAGER HEAD -->/""$gtmHead"/g
+replaceBody=$"s/<!-- REPLACE-ME: GOOGLE TAG MANAGER BODY -->/""$gtmBody"/g
+
+echo "Begin replace head"
+sed -i  "$replaceHead" spa/src/index.html
+
+echo "Begin replace body"
+sed -i  "$replaceBody" spa/src/index.html
+
 
 
 echo \"Deleting ./dist/\"
 rm -rf ./dist
 
-./insert-gtm-snippet.sh
+
 cd spa
 
 
