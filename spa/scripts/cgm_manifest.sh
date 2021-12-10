58c45292a285a2e6ca365f12d4ad9540112c70fd
Arctenik <17056247+Arctenik@users.noreply.github.com>
2021-12-09 22:41:52 -0800

    Add support for ZIP files to update-archive-previews

--- spa/scripts/cgm_manifest.sh
+++ spa/scripts/cgm_manifest.sh
@@ -4,6 +4,8 @@ function usage () {
 
 set -e
 
+files_dir="extracted"
+
 if [ $# != 3 ]
 then
    usage
@@ -11,19 +13,41 @@ then
    exit 1
 fi
 
-tar -xf $3  # untar the contents
-tar_file_size=$(wc -c $3 | tr -s ' ' | cut -f1 -d ' ')
-files=$(tar -tf $3)
+mkdir -p "$files_dir"
+
+# extract contents and get file list for different archive types
+if [[ "$3" =~ \.[zZ][iI][pP](\.|$) ]]
+then
+	if [[ "$3" =~ \.[gG][zZ]$ ]]
+	then
+		zip_name="$3_ungz.zip"
+		gzip -dc "$3" > "$zip_name"
+	else
+		zip_name="$3"
+	fi
+	
+	files=$(unzip -Z1 "$zip_name")
+	unzip -q -d "$files_dir" "$zip_name"
+	
+	if [ ! "$zip_name" == "$3" ]
+		then rm "$zip_name"
+	fi
+else
+	files=$(tar -tf $3)
+	tar -xf $3 -C "$files_dir"
+fi
+
+archive_file_size=$(wc -c "$3" | tr -s ' ' | cut -f1 -d ' ')
 output_file="$1-$(echo $2 | tr ':' '_').json"
 first=1
-printf "{\\n  \"archive\": {\\n    \"name\": \"$3\",\\n    \"size\": $tar_file_size,\\n    \"uuid\": \"$1\",\\n    \"version\": \"$2\"\\n  },\\n  \"files\":[\\n" > $output_file
+printf "{\\n  \"archive\": {\\n    \"name\": \"$3\",\\n    \"size\": $archive_file_size,\\n    \"uuid\": \"$1\",\\n    \"version\": \"$2\"\\n  },\\n  \"files\":[\\n" > $output_file
 
-for file in $files
+while IFS= read -r file
 do
    if [ ! -d "$file" ]  # Don't include directories in the manifest 
       then
-        fdate=$(date -r  $file -u +"%Y-%m-%dT%H:%M:%SZ")
-        fsize=$(wc -c $file | tr -s ' ' | cut -f1 -d ' ')
+        fdate=$(date -r  "$files_dir/$file" -u +"%Y-%m-%dT%H:%M:%SZ")
+        fsize=$(wc -c "$files_dir/$file" | tr -s ' ' | cut -f1 -d ' ')
         if [ $first -eq 1 ]
            then
              first=0
@@ -32,6 +56,6 @@ do
         fi
         printf "    {\\n      \"name\":\"$file\",\n      \"size\": $fsize,\n      \"modified\": \"$fdate\"\n    }"  >> $output_file
    fi
-done
+done <<< "$files"
 
 printf "\n  ]\n}\n" >> $output_file
