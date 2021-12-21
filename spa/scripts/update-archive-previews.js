c7a3f8a7b30b5b0b3cd30e537f5c7e21dcace286
Arctenik <17056247+Arctenik@users.noreply.github.com>
2021-12-21 15:10:07 -0800

    Update update-archive-previews

--- spa/scripts/update-archive-previews.js
+++ spa/scripts/update-archive-previews.js
@@ -24,14 +24,14 @@ const client = new S3Client({ region: "us-east-1" });
 	
 	const {hits} = await got(hcaApiUrl).json();
 	
-	let sizeLimit = 1000000,
+	let sizeLimit = 10000000,
 		countLimit = 5;
 	
 	for (let entry of hits) {
 		for (let file of entry.files) {
 			if (file.size < sizeLimit) {
 				await processFile(file);
-				if (countLimit-- <= 0) break;
+				if (--countLimit <= 0) break;
 			}
 		}
 		if (countLimit <= 0) break;
@@ -45,6 +45,11 @@ async function processFile(file) {
 	console.log(file);
 		
 	const jsonName = file.uuid + "_" + file.version.replace(/:/g, "") + ".json";
+	const jsonPath = path.resolve(outPath, jsonName);
+	
+	if (jsonPath.substring(0, outPath.length + 1) !== outPath + path.sep) {
+		throw new Error("Invalid JSON name");
+	}
 	
 	const headCommand = new HeadObjectCommand({
 		Bucket: bucketName,
@@ -59,52 +64,41 @@ async function processFile(file) {
 		jsonExists = false;
 	}
 	
-	if (jsonExists) {
-		console.log("JSON exists");
-	} else {
+	if (!jsonExists) {
 		const fetchUrl = file.url.replace("https://service.azul.data.humancellatlas.org/repository/files/", "https://service.azul.data.humancellatlas.org/fetch/repository/files/");
 		
 		const {Location: fileUrl} = await got(fetchUrl).json();
 		
-		//console.log(fileUrl);
-		
 		const fileDlName = file.name || file.uuid + "." + file.format;
 		const fileDlPath = path.resolve(outPath, fileDlName);
 		
-		if (fileDlPath.substring(0, outPath.length + 1) === outPath + path.sep) {
-			await pipeline(
-				got.stream(fileUrl),
-				fs.createWriteStream(fileDlPath)
-			);
-			
-			//console.log("Processing");
-			const jsonPath = path.resolve(outPath, jsonName);
-			
-			if (jsonPath.substring(0, outPath.length + 1) === outPath + path.sep) {
-				// should this make sure there's nothing weird about the strings passed to it?
-				await exec(
-					`"../spa/scripts/cgm_manifest.sh" ${JSON.stringify(file.uuid)} ${JSON.stringify(file.version)} ${JSON.stringify(fileDlName)}`,
-					{cwd: outPath}
-				);
-				
-				const putCommand = new PutObjectCommand({
-					Bucket: bucketName,
-					Key: jsonName,
-					Body: await fsPromises.readFile(jsonPath)
-				});
-				
-				const results = await client.send(putCommand);
-				//console.log(results);
-				
-				await fsPromises.rm(fileDlPath);
-				await fsPromises.rm(jsonPath);
-				
-				console.log("Processed file");
-			} else {
-				console.log("Invalid JSON name");
-			}
-		} else {
-			console.log("Invalid file name");
+		if (fileDlPath.substring(0, outPath.length + 1) !== outPath + path.sep) {
+			throw new Error("Invalid archive name");
 		}
+		
+		await pipeline(
+			got.stream(fileUrl),
+			fs.createWriteStream(fileDlPath)
+		);
+		
+		await exec(
+			`"../spa/scripts/cgm_manifest.sh" ${JSON.stringify(file.uuid)} ${JSON.stringify(file.version)} ${JSON.stringify(fileDlName)}`,
+			{cwd: outPath}
+		);
+		
+		const putCommand = new PutObjectCommand({
+			Bucket: bucketName,
+			Key: jsonName,
+			Body: await fsPromises.readFile(jsonPath)
+		});
+		
+		const results = await client.send(putCommand);
+		
+		await fsPromises.rm(fileDlPath);
+		await fsPromises.rm(jsonPath);
+		
+		console.log("Processed file");
+	} else {
+		console.log("JSON exists");
 	}
 }
