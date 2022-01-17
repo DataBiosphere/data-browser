c60711994516d4e2c01315bf9fa8a78e74f453d9
Fran McDade <franmcdade@Frans-MBP-2.hub>
2022-01-17 16:50:21 +1000

    Add social icons/links to top nav/header (Browser). #2160.

---
+++ spa/src/app/shared/toolbar-socials/toolbar-socials.component.ts
@@ -0,0 +1,18 @@
+/*
+ * Human Cell Atlas
+ * https://www.humancellatlas.org/
+ *
+ * Component for displaying toolbar socials inside a toolbar.
+ */
+
+// Dependencies
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "toolbar-socials",
+    templateUrl: "toolbar-socials.component.html",
+    styleUrls: ["toolbar-socials.component.scss"]
+})
+
+export class ToolbarSocialsComponent {
+}
