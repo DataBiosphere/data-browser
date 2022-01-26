9b56ba34faa8c39624c36fe939f712c685bea0e6
Arctenik <17056247+Arctenik@users.noreply.github.com>
2022-01-25 21:29:27 -0800

    Update get-missing-descriptions

--- spa/scripts/get-missing-descriptions.js
+++ spa/scripts/get-missing-descriptions.js
@@ -1,9 +1,12 @@
 
 
 const fs = require("fs");
+const fsPromises = require("fs/promises");
+const path = require("path");
 const got = require("got");
 
-const csvOutPath = "./missing-description-files.csv";
+const outDir = "../../temp";
+const csvOutName = "missing-description-files.csv";
 const projectsUrl = "https://service.azul.data.humancellatlas.org/index/projects?catalog=dcp12&size=500";
 const filesUrl = "https://service.azul.data.humancellatlas.org/index/files?filters=%7B%22contentDescription%22%3A%7B%22is%22%3A%5Bnull%5D%7D%7D&size=500&catalog=dcp12";
 const latticeEmail = "lattice-info@lists.stanford.edu";
@@ -13,8 +16,10 @@ const latticeEmail = "lattice-info@lists.stanford.edu";
 	console.log("Getting projects");
 	
 	const latticeProjects = new Set();
+	let numProjects = 0;
 	
 	for await (const { projects: [project] } of getSearchResults(projectsUrl)) {
+		numProjects++;
 		for (const contrib of project.contributors) {
 			if (contrib.email === latticeEmail) {
 				latticeProjects.add(project.projectId);
@@ -23,23 +28,34 @@ const latticeEmail = "lattice-info@lists.stanford.edu";
 		}
 	}
 	
-	const writeStream = fs.createWriteStream(csvOutPath);
+	try {
+		await fsPromises.mkdir(outDir);
+	} catch(e) {
+		// assume the directory already exists
+	}
+	
+	const writeStream = fs.createWriteStream(path.resolve(outDir, csvOutName));
 	
 	writeStream.write("Project,Project UUID,Ingest,File name,File extension,File UUID,File version");
 	
-	//const pageLimit = 10;
+	//const pageLimit = 5;
 	
 	console.log("Getting files");
 	
+	const missingDescriptionProjects = new Set();
+	let numMDLatticeProjects = 0;
 	let pageNum = 1;
 	
 	for await (const page of getSearchResults(filesUrl, true)) {
 		console.log("Page " + (pageNum++) + "/" + page.pagination.pages);
 		for (const { files: [file], projects: [project] } of page.hits) {
+			const projectId = project.projectId[0];
+			missingDescriptionProjects.add(projectId);
+			if (latticeProjects.has(projectId)) numMDLatticeProjects++;
 			const items = [
-				project.projectTitle[0],
-				project.projectId[0],
-				latticeProjects.has(project.projectId[0]) ? "Lattice" : "EBI",
+				project.projectShortname[0],
+				projectId,
+				latticeProjects.has(projectId) ? "Lattice" : "EBI",
 				file.name,
 				file.format,
 				file.uuid,
@@ -53,7 +69,7 @@ const latticeEmail = "lattice-info@lists.stanford.edu";
 	
 	writeStream.end();
 	
-	console.log("Done");
+	console.log("Done - Found " + missingDescriptionProjects.size + " projects (" + numMDLatticeProjects + " Lattice, " + (missingDescriptionProjects.size - numMDLatticeProjects) + " EBI) with missing file content descriptions, out of " + numProjects + " total projects");
 })();
 
 
