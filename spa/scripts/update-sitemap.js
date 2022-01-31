431badae256a939869f3fa9c2e40c7610c015d92
David Rogers <dave@clevercanary.com>
2022-01-30 23:17:19 -0800

    Task sitemap 220124 for #2203 (#2211)
    * Create update-sitemap script
    
    * Update update-sitemap with new output path
    
    * Update update-sitemap output path
    
    * Update update-sitemap output path
    
    Co-authored-by: Arctenik <17056247+Arctenik@users.noreply.github.com>

---
+++ spa/scripts/update-sitemap.js
@@ -0,0 +1,62 @@
+
+
+const { promisify } = require("util");
+const fs = require("fs");
+const got = require("got");
+const { SitemapStream } = require("sitemap");
+
+const pipeline = promisify(require("stream").pipeline);
+
+const sitemapOutPath = "../../dist/sitemap.xml";
+
+const scriptArgs = {};
+
+process.argv.slice(2).forEach((arg) => {
+    const [key, value] = arg.split("=");
+    scriptArgs[key] = value;
+});
+
+var hcaApiUrl;
+
+// TODO: add stuff for the "env" argument
+
+if (!scriptArgs.catalog) {
+	console.log("Missing/invalid arguments");
+	console.log("Usage: npm run-script update-sitemap -- catalog=<catalog> env={dev|prod}");
+} else (async function() {
+	hcaApiUrl = "https://service.azul.data.humancellatlas.org/index/projects?catalog=" + scriptArgs.catalog + "&size=500"
+	
+	const stream = new SitemapStream({ hostname: "https://data.humancellatlas.org/" });
+	
+	for (let entry of await getSearchResults(hcaApiUrl)) {
+		for (let project of entry.projects) {
+			stream.write({
+				url: "/explore/projects/" + project.projectId,
+				lastmod: project.lastModifiedDate,
+				changefreq: "monthly"
+			});
+		}
+	}
+	
+	const pl = pipeline(stream, fs.createWriteStream(sitemapOutPath));
+	stream.end();
+	await pl;
+	
+	console.log("Done");
+})();
+
+
+async function getSearchResults(url) {
+    // make a request using the url, paginate through the results, and return a combined results array
+
+    const allHits = [];
+
+    while (url) {
+        const { hits, pagination } = await got(url).json();
+        allHits.push(...hits);
+        url = pagination.next;
+    }
+
+    return allHits;
+}
+
