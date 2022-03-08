8ac581e57b8e3cc0933c9a64d21ae0e33f5500d2
MillenniumFalconMechanic <mim@clevercanary.com>
2022-03-08 10:15:36 -0800

    Added LungMAP analysis portals. #2271

--- spa/src/app/files/project-analysis-portal/project-analysis-portal-name.model.ts
+++ spa/src/app/files/project-analysis-portal/project-analysis-portal-name.model.ts
@@ -9,5 +9,6 @@ export enum ProjectAnalysisPortalName {
     "CELL_BROWSER" = "UCSC Cell Browser",
     "CELLXGENE" = "cellxgene",
     "GENOME_BROWSER" = "UCSC Genome Browser",
+    "LUNGMAP_APPS" = "LungMAP Apps",
     "STEM_CELL_HUB" = "The Stem Cell Hub"
 }
