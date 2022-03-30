fb63e7a7c43ecd3d041789290574db543908e4bc
Dave Rogers <dave@clevercanary.com>
2022-03-30 10:49:16 -0700

    Default catalog to dcp14 for 2347

--- spa/scripts/update-archive-previews.js
+++ spa/scripts/update-archive-previews.js
@@ -46,7 +46,7 @@ let client;
         });
     } else {
         hcaApiUrl =
-            "https://service.azul.data.humancellatlas.org/index/files?filters=%7B%22fileFormat%22%3A%7B%22is%22%3A%5B%22zip%22%2C%22zip.gz%22%2C%22tar%22%2C%22tar.gz%22%5D%7D%7D&size=500&catalog=dcp13";
+            "https://service.azul.data.humancellatlas.org/index/files?filters=%7B%22fileFormat%22%3A%7B%22is%22%3A%5B%22zip%22%2C%22zip.gz%22%2C%22tar%22%2C%22tar.gz%22%5D%7D%7D&size=500&catalog=dcp14";
         if (env === "test") {
             bucketName = "cc-archive-preview-test";
             client = new S3Client({ region: "us-east-1" });
@@ -57,7 +57,7 @@ let client;
                 credentials: fromIni({
                     profile: "platform-hca-prod",
                     mfaCodeProvider: async (mfaSerial) => {
-                        return "451871";
+                        return "327428";
                     },
                 }),
             });
