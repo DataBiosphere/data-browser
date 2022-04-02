d1af7b010df7496cd0e42366b91548dc016c12b2
Arctenik <17056247+Arctenik@users.noreply.github.com>
2022-04-02 12:34:37 -0700

    Add download type and ethnicity graphs to analytics book

--- analytics/hdgar-book/analytics_charts.py
+++ analytics/hdgar-book/analytics_charts.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from google.cloud import bigquery
+import json
+import requests
 
 
 def authenticate_ga():
@@ -121,16 +123,129 @@ def plot_hbar(ga_property, title, xlabel, ylabel, metric, dimension, start_date,
 
 
 def plot_downloads():
+	QUERY = """
+		SELECT
+			day,
+			COUNT(*) AS totalSessionCount,
+			COUNTIF("fastq" IN UNNEST(fileTypes)) AS fastqSessionCount,
+			COUNTIF("bam" IN UNNEST(fileTypes)) AS bamSessionCount,
+			COUNTIF("matrix" IN UNNEST(fileTypes)) AS matrixSessionCount,
+			COUNTIF("other" IN UNNEST(fileTypes)) AS otherSessionCount,
+			COUNTIF(".loom" IN UNNEST(matrixTypes)) AS loomMatrixSessionCount,
+			COUNTIF(ARRAY_LENGTH(matrixTypes) > 1 OR (ARRAY_LENGTH(matrixTypes) = 1 AND matrixTypes[OFFSET(0)] != ".loom")) AS otherMatrixSessionCount
+		FROM (
+			SELECT
+				DATE_TRUNC(timestamp, DAY) AS day,
+				protopayload_auditlog.requestMetadata.callerIp,
+				ARRAY_AGG(
+					DISTINCT
+					IF(
+						REGEXP_CONTAINS(protopayload_auditlog.resourceName, "(?i)\\\\.fastq(?:\\\\.|$)"),
+						"fastq",
+						IF(
+							REGEXP_CONTAINS(protopayload_auditlog.resourceName, "(?i)\\\\.bam(?:\\\\.|$)"),
+							"bam",
+							IF(
+								REGEXP_CONTAINS(protopayload_auditlog.resourceName, "(?i)\\\\.(?:h5ad|loom|mtx)(?:\\\\.|$)"),
+								"matrix",
+								"other"
+							)
+						)
+					)
+				) AS fileTypes,
+				ARRAY_AGG(
+					DISTINCT
+					REGEXP_EXTRACT(protopayload_auditlog.resourceName, "(?i)\\\\.(?:(?:h5ad|mtx)(?:\\\\.|$)|loom$|loom\\\\.)")
+					IGNORE NULLS
+				) AS matrixTypes
+			FROM `broad-datarepo-terra-prod-hca2.accesslogs.cloudaudit_googleapis_com_data_access_*`
+			GROUP BY day, callerIp
+		)
+		GROUP BY day ORDER BY day
+	"""
+
 	client = bigquery.Client()
 
-	QUERY = (
-		'WITH days AS '
-		'(SELECT protopayload_auditlog.requestMetadata.callerIp, DATE_TRUNC(timestamp, DAY) as day FROM `broad-datarepo-terra-prod-hca2.accesslogs.cloudaudit_googleapis_com_data_access_*`) ' 
-		'SELECT day, COUNT(DISTINCT callerIp) as ipCount FROM days GROUP BY day ORDER BY day')
+	df = client.query(QUERY).result().to_dataframe(create_bqstorage_client=False)
+	df.set_index("day", inplace=True)
+
+	ax_total = df["totalSessionCount"].plot(title='Number of sessions per day downloading files', legend=False, xlabel='')
+	plt.show()
+	
+	df_a = df[["fastqSessionCount", "bamSessionCount", "matrixSessionCount", "otherSessionCount"]].rolling("7d").mean()
+	
+	axs_a = df_a.plot(subplots=True, ylim=(0, df_a.max().max() + 5), title="Number of sessions per day downloading given file types", xlabel="")
+	axs_a[0].get_figure().set_size_inches(7, 6)
+	axs_a[0].legend(["FASTQ"])
+	axs_a[1].legend(["BAM"])
+	axs_a[2].legend(["Matrix"])
+	axs_a[3].legend(["Other"])
+	plt.tight_layout()
+	plt.show()
+
+	df_b = pd.concat([
+		(df["matrixSessionCount"] - df["loomMatrixSessionCount"]).rename("CGMs only"),
+		(df["loomMatrixSessionCount"] + df["otherMatrixSessionCount"] - df["matrixSessionCount"]).rename("CGMs and DCP matrices"),
+		(df["matrixSessionCount"] - df["otherMatrixSessionCount"]).rename("DCP matrices only")
+	], axis=1).rolling("7d").mean()
+
+	ax_b = df_b.plot(kind="area", stacked=True, title="Number of sessions per day downloading matrices", xlabel="")
+	ax_b.get_figure().set_size_inches(7, 5)
+	plt.tight_layout()
+	plt.show()
+
+def plot_ethnicity():
+	catalog = "dcp13"
+
+	def get_sources_expr():
+		sources = requests.get("https://service.azul.data.humancellatlas.org/repository/sources?catalog=" + catalog).json()["sources"]
+		return "(" + " UNION ALL ".join(["SELECT * FROM `" + get_table_name(source["sourceSpec"]) + "`" for source in sources]) + ")"
+
+	def get_table_name(spec):
+		parts = [p.split(":") for p in spec.split("/")]
+		return parts[0][1] + "." + parts[1][0] + ".donor_organism"
+
+	QUERY = """
+		select
+			IFNULL(ethnicity, "[Missing]") as Ethnicity,
+			Code,
+			count(*) as Count
+		from (
+			select
+				datarepo_row_id,
+				json_extract_scalar(gs, "$.ontology") as species,
+				json_extract_scalar(eth, "$.ontology_label") as ethnicity, 
+				json_extract_scalar(eth, "$.ontology") as Code,
+				content,
+				json_extract_scalar(content, "$.provenance.document_id") AS did
+			from
+				%s
+				LEFT JOIN UNNEST(JSON_EXTRACT_ARRAY(content, "$.genus_species")) as gs
+				LEFT JOIN UNNEST(JSON_EXTRACT_ARRAY(content, "$.human_specific.ethnicity")) AS eth
+		) as T
+		where species = "NCBITaxon:9606"
+		GROUP BY T.ethnicity, T.Code
+		ORDER BY Count DESC 
+		""" % get_sources_expr()
+	
+	client = bigquery.Client()
 
 	df = client.query(QUERY).result().to_dataframe(create_bqstorage_client=False)
-	df.set_index('day', inplace=True)
-
-	ax = df.plot(title='Number of sessions per day downloading files', legend=False, xlabel='')
+	df.set_index("Ethnicity", inplace=True)
+	
+	series_a = df["Count"].groupby(lambda eth: "Unspecified" if eth == "[Missing]" else "Specified").sum()
+	series_b = df.loc[df.index != "[Missing]"]["Count"]
+	
+	series_a.sort_values(inplace=True, ascending=False)
+	
+	ax_a = series_a.plot(kind="barh", title="Donor ethnicity (" + catalog.upper() + ") - Specified vs. Unspecified", xlabel="")
+	ax_a.set_xlabel("Donors")
+	plt.tight_layout()
+	plt.show()
+	
+	ax_b = series_b.plot(kind="barh", title="Donor ethnicity (" + catalog.upper() + ")", xlabel="")
+	ax_b.get_figure().set_size_inches(10, 6)
+	ax_b.set_xlabel("Donors")
+	plt.tight_layout()
 	plt.show()
 
