61e133e06e9774a9e2c982035a11b355ab67679d
David Rogers <dave@clevercanary.com>
2022-04-20 14:09:16 -0700

    Update default catalog to dcp15 for #2458 (#2463)

--- spa/src/app/files/catalog/catalog-display-name.model.ts
+++ spa/src/app/files/catalog/catalog-display-name.model.ts
@@ -8,17 +8,7 @@
 export enum CatalogDisplayName {
     "dcp1" = "DCP 1.0 Data View",
     "dcp2" = "DCP 2.0 Data View",
-    "dcp3" = "DCP 2.0 Data View",
-    "dcp4" = "DCP 2.0 Data View",
-    "dcp5" = "DCP 2.0 Data View",
-    "dcp6" = "DCP 2.0 Data View",
-    "dcp7" = "DCP 2.0 Data View",
-    "dcp8" = "DCP 2.0 Data View",
-    "dcp9" = "DCP 2.0 Data View",
-    "dcp10" = "DCP 2.0 Data View",
-    "dcp11" = "DCP 2.0 Data View",
-    "dcp12" = "DCP 2.0 Data View",
-    "dcp13" = "DCP 2.0 Data View",
     "dcp14" = "DCP 2.0 Data View",
+    "dcp15" = "DCP 2.0 Data View",
     "dcp2ebi" = "DCP2EBI",
 }
--- spa/src/environments/environment.dcp2.ts
+++ spa/src/environments/environment.dcp2.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     atlas: "hca",
     dataURL: "https://service.azul.data.humancellatlas.org",
-    defaultCatalog: "dcp14",
+    defaultCatalog: "dcp15",
     browserURL: "https://data.humancellatlas.org",
     googleOAuthClientId: "",
     portalURL: "https://data.humancellatlas.org",
