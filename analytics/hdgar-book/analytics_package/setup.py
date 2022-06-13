3c75f58e17bbbfd7a29efa24eb7e50c371768ece
Arctenik <17056247+Arctenik@users.noreply.github.com>
2022-06-13 16:53:23 -0700

    Update setup.py

--- analytics/hdgar-book/analytics_package/setup.py
+++ analytics/hdgar-book/analytics_package/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
 	name="analytics",
 	version="1.0",
-	py_modules=["charts", "api"],
+	package_dir={"analytics": ""},
+	py_modules=["analytics.charts", "analytics.api"],
 	install_requires=["matplotlib", "pandas", "numpy", "google-auth-oauthlib", "google-api-python-client"],
 )
