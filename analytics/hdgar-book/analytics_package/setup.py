b1509527341f7e22b9b72c870ac0bee455f701b5
Arctenik <17056247+Arctenik@users.noreply.github.com>
2022-06-13 17:11:21 -0700

    Move to package subfolder

--- analytics/hdgar-book/analytics_package/setup.py
+++ analytics/hdgar-book/analytics_package/setup.py
@@ -3,8 +3,6 @@ from setuptools import setup
 setup(
 	name="analytics",
 	version="1.0",
-	package_dir={"analytics": ""},
 	packages=["analytics"],
-	py_modules=["analytics.charts", "analytics.api"],
 	install_requires=["matplotlib", "pandas", "numpy", "google-auth-oauthlib", "google-api-python-client"],
 )
