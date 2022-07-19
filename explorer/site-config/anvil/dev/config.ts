99cb4dd2b0b768d383773e9bbd4e28c80210462a
Mim Hastie <mim@clevercanary.com>
2022-07-18 21:53:23 -0700

    Updated AnVIL endpoint to 2. #234. (#243)

--- explorer/site-config/anvil/dev/config.ts
+++ explorer/site-config/anvil/dev/config.ts
@@ -61,7 +61,7 @@ const config: SiteConfig = {
       size: "25",
       sort: "entryId",
     },
-    url: "https://service.nadove3.dev.singlecell.gi.ucsc.edu/",
+    url: "https://service.nadove2.dev.singlecell.gi.ucsc.edu/",
   },
   entities: [
     datasetsEntity,
