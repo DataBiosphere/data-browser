c386476d90452a1a27807a545a04e7e6b54ca323
Dave Rogers <dave@clevercanary.com>
2022-07-21 10:35:29 -0700

    Change default catalog to dcp18 for #2707

--- spa/src/environments/environment.dcp2.ts
+++ spa/src/environments/environment.dcp2.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     atlas: "hca",
     dataURL: "https://service.azul.data.humancellatlas.org",
-    defaultCatalog: "dcp17",
+    defaultCatalog: "dcp18",
     browserURL: "https://data.humancellatlas.org",
     googleOAuthClientId: "",
     portalURL: "https://data.humancellatlas.org",
