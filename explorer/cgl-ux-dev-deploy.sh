30a54e83e7b496de332132946eb6b35b6ae9a997
Dave Rogers <dave@clevercanary.com>
2022-07-22 10:21:54 -0700

    Added build and deploy script for #302

---
+++ explorer/cgl-ux-dev-deploy.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+export GATSBY_GTM_ID=GTM-M2J5NTJ
+export GATSBY_GTM_AUTH=CzDpc0__fhyqfREDehPK8Q
+export GATSBY_ENV_NAME=env-83
+
+
+echo \"Deleting ./out/\"
+rm -rf ./out
+
+echo \"Deleting ./build/\"
+rm -rf ./build
+
+#./insert-gtm-snippet.sh
+
+mkdir -p build/explore/anvil
+mkdir -p build/explore/hca
+mkdir -p build/explore/lungmap
+
+# Build AnVIL
+rm -rf ./out
+npm run build:anvil
+mv out/* build/explore/anvil
+
+# Build HCA
+rm -rf ./out
+npm run build:hca-dcp
+mv out/* build/explore/hca
+
+
+rm -rf ./out
+npm run build:lungmap
+mv out/* build/explore/lungmap
+
+
+export BUCKET=s3://ux-dev.explore.singlecell.gi.ucsc.edu/
+export SRCDIR=build/
+
+aws s3 sync --acl public-read $SRCDIR $BUCKET --delete --profile ucsc-cgl
+aws cloudfront create-invalidation --distribution-id E3FFK49Z7TQ60R --paths "/*" --profile ucsc-cgl
