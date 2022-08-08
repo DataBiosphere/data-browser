161cd8464eebdeddc49ae123202619f3702b8d0b
MillenniumFalconMechanic <mim@clevercanary.com>
2022-08-08 12:09:31 -0700

    Updated analysis portals. #2732

--- spa/src/app/files/project-analysis-portal/project-analysis-portal-name.model.ts
+++ spa/src/app/files/project-analysis-portal/project-analysis-portal-name.model.ts
@@ -9,6 +9,9 @@ export enum ProjectAnalysisPortalName {
     "CELL_BROWSER" = "UCSC Cell Browser",
     "CELLXGENE" = "cellxgene",
     "GENOME_BROWSER" = "UCSC Genome Browser",
+    "LGEA" = "LGEA",
     "LUNGMAP_APPS" = "LungMAP Apps",
+    "SHINY" = "Shiny",
     "STEM_CELL_HUB" = "The Stem Cell Hub",
+    "TOPPCELL" = "ToppCell",
 }
