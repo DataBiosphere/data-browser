656bbf70c2a5dbad979a62c64f5c9092dc64bfd8
Arctenik <17056247+Arctenik@users.noreply.github.com>
2022-08-16 00:02:49 -0700

    Update analytics package version number

--- analytics/hdgar-book/analytics_package/setup.py
+++ analytics/hdgar-book/analytics_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="analytics",
-	version="1.4",
+	version="1.5",
 	packages=["analytics"],
 	install_requires=["matplotlib", "pandas", "numpy", "google-auth-oauthlib", "google-api-python-client"],
 )
