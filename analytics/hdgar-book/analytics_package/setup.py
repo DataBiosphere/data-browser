7c0feb0365a2985e41f2eacac1079d2debb1d0ad
Arctenik <17056247+Arctenik@users.noreply.github.com>
2022-08-16 17:25:52 -0700

    Support `max_results` and `sort` in API requests

--- analytics/hdgar-book/analytics_package/setup.py
+++ analytics/hdgar-book/analytics_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="analytics",
-	version="1.5.3",
+	version="1.5.4",
 	packages=["analytics"],
 	install_requires=["matplotlib", "pandas", "numpy", "google-auth-oauthlib", "google-api-python-client"],
 )
