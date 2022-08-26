c9963145ac4d2f3ae3151301d00d20b080f76704
Arctenik <17056247+Arctenik@users.noreply.github.com>
2022-08-26 13:56:47 -0700

    Fix number conversion to account for floats

--- analytics/hdgar-book/analytics_package/setup.py
+++ analytics/hdgar-book/analytics_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="analytics",
-	version="1.7",
+	version="1.7.1",
 	packages=["analytics"],
 	install_requires=["matplotlib", "pandas", "numpy", "google-auth-oauthlib", "google-api-python-client"],
 )
