615cc0b5159cd10215a662052d27abfeb93a175a
Arctenik <17056247+Arctenik@users.noreply.github.com>
2022-08-29 00:48:08 -0700

    Allow alternative dimensions for plots over time

--- analytics/hdgar-book/analytics_package/setup.py
+++ analytics/hdgar-book/analytics_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="analytics",
-	version="1.7.1",
+	version="1.8",
 	packages=["analytics"],
 	install_requires=["matplotlib", "pandas", "numpy", "google-auth-oauthlib", "google-api-python-client"],
 )
