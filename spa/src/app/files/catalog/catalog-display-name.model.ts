1464cb1077cd4e33ff92613db21e85d3311bf29f
David Rogers <dave@clevercanary.com>
2022-09-24 10:34:10 -0700

    Update default catalog to dcp20 for #2851 (#2863)

--- spa/src/app/files/catalog/catalog-display-name.model.ts
+++ spa/src/app/files/catalog/catalog-display-name.model.ts
@@ -8,7 +8,7 @@
 export enum CatalogDisplayName {
     "dcp1" = "DCP 1.0 Data View",
     "dcp2" = "DCP 2.0 Data View",
-    "dcp18" = "DCP 2.0 Data View",
     "dcp19" = "DCP 2.0 Data View",
+    "dcp20" = "DCP 2.0 Data View",
     "dcp2ebi" = "DCP2EBI",
 }
