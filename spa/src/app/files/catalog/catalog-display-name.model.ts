25a7360e31045a2dcd991fca270021c34378b421
Dave Rogers <dave@clevercanary.com>
2022-11-30 21:42:44 -0800

    update catalog to dcp22 for #2984

--- spa/src/app/files/catalog/catalog-display-name.model.ts
+++ spa/src/app/files/catalog/catalog-display-name.model.ts
@@ -8,8 +8,7 @@
 export enum CatalogDisplayName {
     "dcp1" = "DCP 1.0 Data View",
     "dcp2" = "DCP 2.0 Data View",
-    "dcp19" = "DCP 2.0 Data View",
-    "dcp20" = "DCP 2.0 Data View",
     "dcp21" = "DCP 2.0 Data View",
+    "dcp22" = "DCP 2.0 Data View",
     "dcp2ebi" = "DCP2EBI",
 }
--- spa/src/environments/environment.dcp2.ts
+++ spa/src/environments/environment.dcp2.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     atlas: "hca",
     dataURL: "https://service.azul.data.humancellatlas.org",
-    defaultCatalog: "dcp21",
+    defaultCatalog: "dcp22",
     browserURL: "https://data.humancellatlas.org",
     googleOAuthClientId: "",
     portalURL: "https://data.humancellatlas.org",
