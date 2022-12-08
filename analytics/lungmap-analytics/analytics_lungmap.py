09953c673eba3ae32dc4ce0a59144461fc64b015
Arctenik <17056247+Arctenik@users.noreply.github.com>
2022-12-07 18:03:49 -0800

    Update analytics for November

--- analytics/lungmap-analytics/analytics_lungmap.py
+++ analytics/lungmap-analytics/analytics_lungmap.py
@@ -1,8 +1,25 @@
 import analytics.charts as ac
+import json
+import re
 from html import escape as escape_html
+
+def format_export_url_info(type, secondary_type, filter):
+	result = escape_html(type.replace("-", " "))
+	if secondary_type:
+		result += escape_html(", " + secondary_type.replace("-", " "))
+	for facet in json.loads(filter):
+		if facet["facetName"] == "projectId":
+			result += "\nProject: " + ", ".join(['<a href="https://data-browser.lungmap.net/explore/projects/' + id + '">' + id + '</a>' for id in facet["terms"]])
+		else:
+			result += escape_html("\n" + facet["facetName"] + ": " + ", ".join([term if isinstance(term, str) else json.dumps(term) for term in facet["terms"]]))
+	return (result, True)
 
 def adjust_table_index_key(val):
 	if isinstance(val, str):
+		match = re.search("^\\/explore\\/(?:projects\\/([^\\/#?]+)|export\\/(export-to-terra|get-curl-command|download-manifest)(?:\\/(select-species))?\\?filter=(.+))", val)
+		if match:
+			if not match.group(1):
+				return format_export_url_info(match.group(2), match.group(3), match.group(4))
 		if val[0] == "/":
 			return ('<a href="' + escape_html("https://data-browser.lungmap.net" + val) + '">' + escape_html(val) + '</a>', True)
 	return val
