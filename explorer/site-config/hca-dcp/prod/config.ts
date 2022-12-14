28306ae18cb69a234141f58e1d84327c4bfd5232
Dave Rogers <dave@clevercanary.com>
2022-12-14 13:05:17 -0800

    Point hca-dcp to dev for #635

--- explorer/site-config/hca-dcp/prod/config.ts
+++ explorer/site-config/hca-dcp/prod/config.ts
@@ -7,16 +7,6 @@ const BROWSER_URL = "https://data.humancellatlas.org";
 const config: SiteConfig = {
   ...devConfig,
   browserURL: BROWSER_URL,
-  dataSource: {
-    defaultDetailParams: {
-      catalog: "dcp21",
-    },
-    defaultListParams: {
-      catalog: "dcp21",
-      size: "25",
-    },
-    url: "https://service.azul.data.humancellatlas.org/",
-  },
   exportToTerraUrl: "https://app.terra.bio/",
 };
 
