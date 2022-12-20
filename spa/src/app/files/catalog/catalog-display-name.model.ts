ceb3c792e18b65257408caa0551452d35b37700f
Dave Rogers <dave@clevercanary.com>
2022-12-19 21:43:43 -0800

    Update catalog to dcp23 for #3013

--- spa/src/app/files/catalog/catalog-display-name.model.ts
+++ spa/src/app/files/catalog/catalog-display-name.model.ts
@@ -8,7 +8,7 @@
 export enum CatalogDisplayName {
     "dcp1" = "DCP 1.0 Data View",
     "dcp2" = "DCP 2.0 Data View",
-    "dcp21" = "DCP 2.0 Data View",
     "dcp22" = "DCP 2.0 Data View",
+    "dcp23" = "",
     "dcp2ebi" = "DCP2EBI",
 }
