df2a067dc4f9790f07deb41502fbf31822b2888f
hunterckx <118154470+hunterckx@users.noreply.github.com>
2023-02-10 13:42:09 -0800

    Use different ports for successive authentications

--- analytics/hdgar-book/analytics_package/setup.py
+++ analytics/hdgar-book/analytics_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="analytics",
-	version="1.8",
+	version="1.9",
 	packages=["analytics"],
 	install_requires=["matplotlib", "pandas", "numpy", "google-auth-oauthlib", "google-api-python-client"],
 )
