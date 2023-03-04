9a7946a7606ff0b82844de6206fd0b5e3b6efaf4
Dave Rogers <dave@clevercanary.com>
2023-03-03 23:31:51 -0800

    fix: set default catalog in environment file

--- spa/src/environments/environment.dcp2.ts
+++ spa/src/environments/environment.dcp2.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     atlas: "hca",
     dataURL: "https://service.azul.data.humancellatlas.org",
-    defaultCatalog: "dcp23",
+    defaultCatalog: "dcp24",
     browserURL: "https://data.humancellatlas.org",
     googleOAuthClientId: "",
     portalURL: "https://data.humancellatlas.org",
