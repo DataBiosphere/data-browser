190e5060ec0eb6d6c0859768de25cae618e5c6ea
Dave Rogers <dave@clevercanary.com>
2023-03-30 15:32:34 -0700

    feat: set default catalog to dcp25 for #3273

--- spa/src/environments/environment.dcp2.ts
+++ spa/src/environments/environment.dcp2.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     atlas: "hca",
     dataURL: "https://service.azul.data.humancellatlas.org",
-    defaultCatalog: "dcp24",
+    defaultCatalog: "dcp25",
     browserURL: "https://data.humancellatlas.org",
     googleOAuthClientId: "",
     portalURL: "https://data.humancellatlas.org",
