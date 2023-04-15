ee855705051db32c617814677cb64d9161fc5c19
Bruce Rodrigues <brucerodrigues6@gmail.com>
2023-04-15 10:05:30 -0300

    #819 - BasePath for the outDir

--- explorer/next-sitemap.config.mjs
+++ explorer/next-sitemap.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "/explore";
+
 const siteMapConfig = {
   changefreq: "monthly",
   generateIndexSitemap: false,
-  outDir: "./out/explore",
-  siteUrl: `${process.env.NEXT_PUBLIC_SITEMAP_DOMAIN}${
-    process.env.NEXT_PUBLIC_BASE_PATH || "/explore"
-  }`,
+  outDir: `./out${basePath}`,
+  siteUrl: `${process.env.NEXT_PUBLIC_SITEMAP_DOMAIN}${basePath}`,
 };
 
 export default siteMapConfig;
