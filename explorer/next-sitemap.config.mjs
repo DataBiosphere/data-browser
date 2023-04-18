d8379d23167ec1840f893fa7029a52dae5ed739a
Bruce Rodrigues <brucerodrigues6@gmail.com>
2023-04-18 19:28:14 -0300

    #72 - Excluding sitemap links (#3200)
    * #72 - Excluding sitemap links
    
    * #72 - removing SITEMAP_EXCLUDE from dev .env file

--- explorer/next-sitemap.config.mjs
+++ explorer/next-sitemap.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "/explore";
 
+const SEPARATOR = ",";
+
 const siteMapConfig = {
   changefreq: "monthly",
+  exclude: (process.env.SITEMAP_EXCLUDE ?? "").split(SEPARATOR),
   generateIndexSitemap: false,
   outDir: `./out${basePath}`,
   siteUrl: `${process.env.NEXT_PUBLIC_SITEMAP_DOMAIN}${basePath}`,
