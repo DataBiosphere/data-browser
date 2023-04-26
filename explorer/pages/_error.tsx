da9691df06026a7dc27e7d35b6252a993faa99ae
Bruce Rodrigues <brucerodrigues6@gmail.com>
2023-04-25 22:52:48 -0300

    #68 - Removing error component & importing from data-explorer-ui

--- explorer/pages/_error.tsx
+++ explorer/pages/_error.tsx
@@ -1,5 +1,5 @@
+import { Error as CustomError } from "@clevercanary/data-explorer-ui/lib/components/Error/error";
 import { useConfig } from "@clevercanary/data-explorer-ui/lib/hooks/useConfig";
-import { Error as CustomError } from "../app/components/Error/error";
 
 export default function Error(): JSX.Element {
   const { config } = useConfig();
