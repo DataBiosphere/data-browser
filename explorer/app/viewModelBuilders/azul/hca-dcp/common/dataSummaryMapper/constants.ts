24e72dbc370c31bc9f064f704efc0550f77e691d
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-05-03 20:51:54 +1000

    feat: view project anatomical entity (#787)

--- explorer/app/viewModelBuilders/azul/hca-dcp/common/dataSummaryMapper/constants.ts
+++ explorer/app/viewModelBuilders/azul/hca-dcp/common/dataSummaryMapper/constants.ts
@@ -4,6 +4,7 @@ import { HCA_DCP_CATEGORY_LABEL } from "../../../../../../site-config/hca-dcp/ca
  * Possible set of data summaries.
  */
 export const enum DATA_SUMMARY {
+  ANATOMICAL_ENTITY = "ANATOMICAL_ENTITY", // organ
   GENUS_SPECIES = "GENUS_SPECIES",
   PROJECT_SHORTNAME = "PROJECT_SHORTNAME",
   SAMPLE_ENTITY_TYPE = "SAMPLE_ENTITY_TYPE",
@@ -13,6 +14,7 @@ export const enum DATA_SUMMARY {
  * Display text for project data summaries.
  */
 export const DATA_SUMMARY_DISPLAY_TEXT = {
+  [DATA_SUMMARY.ANATOMICAL_ENTITY]: HCA_DCP_CATEGORY_LABEL.ANATOMICAL_ENTITY, // organ
   [DATA_SUMMARY.GENUS_SPECIES]: "Species",
   [DATA_SUMMARY.PROJECT_SHORTNAME]: "Project Label",
   [DATA_SUMMARY.SAMPLE_ENTITY_TYPE]: HCA_DCP_CATEGORY_LABEL.SAMPLE_ENTITY_TYPE,
