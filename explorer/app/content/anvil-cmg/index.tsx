67175e2a57d6d8bc20e3b18b519926c8da2d8501
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-06-06 17:46:23 +1000

    feat: implement export dataset to terra (#3223)

--- explorer/app/content/anvil-cmg/index.tsx
+++ explorer/app/content/anvil-cmg/index.tsx
@@ -1,4 +1,8 @@
 export { RenderComponent } from "@clevercanary/data-explorer-ui/lib/components/ComponentCreator/components/RenderComponent/renderComponent";
+export { Section } from "../../components/common/MDXMarkdown/components/Section/mdxSection.styles";
+export { default as ExportToTerra } from "../common/exportToTerra.mdx";
+export { default as ExportToTerraSuccess } from "../common/exportToTerraSuccess.mdx";
+export { default as DataReleasePolicy } from "./dataReleasePolicy.mdx";
 export { default as LoginTermsOfService } from "./loginTermsOfService.mdx";
 export { default as LoginText } from "./loginText.mdx";
 export { default as LoginWarning } from "./loginWarning.mdx";
