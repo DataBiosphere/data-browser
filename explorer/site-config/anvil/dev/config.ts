3699b7d8b1d1fca935570e0c5f74698d86857f58
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-08-08 17:18:25 +1000

    fix: anvil catalog export config (#3599)

--- explorer/site-config/anvil/dev/config.ts
+++ explorer/site-config/anvil/dev/config.ts
@@ -5,6 +5,7 @@ import { CATALOG_DEFAULT } from "../../../app/apis/azul/anvil/common/constants";
 import { authenticationConfig } from "./authentication/authentication";
 import { ANVIL_CATEGORY_KEY, ANVIL_CATEGORY_LABEL } from "./category";
 import { socials } from "./constants";
+import { exportConfig } from "./export/export";
 import { activitiesEntityConfig } from "./index/activitiesEntityConfig";
 import { biosamplesEntityConfig } from "./index/biosamplesEntityConfig";
 import { datasetsEntityConfig } from "./index/datasetsEntityConfig";
@@ -92,6 +93,8 @@ const config: SiteConfig = {
     filesEntityConfig,
   ],
   explorerTitle: "Anvil Data Explorer",
+  export: exportConfig,
+  exportToTerraUrl: "https://app.terra.bio",
   layout: {
     footer: {
       logos: [
---
+++ explorer/site-config/anvil/dev/export/export.ts
@@ -0,0 +1,26 @@
+import {
+  ComponentConfig,
+  ExportConfig,
+} from "@clevercanary/data-explorer-ui/lib/config/entities";
+import * as C from "app/components";
+
+export const exportConfig: ExportConfig = {
+  exportMethods: [],
+  staticLoad: true,
+  tabs: [
+    {
+      label: "Choose Export Method",
+      mainColumn: [],
+      route: "/export",
+      sideColumn: [],
+    },
+  ],
+  top: [
+    {
+      component: C.BackPageHero,
+      props: {
+        title: "Choose Export Method",
+      },
+    } as ComponentConfig<typeof C.BackPageHero>,
+  ],
+};
