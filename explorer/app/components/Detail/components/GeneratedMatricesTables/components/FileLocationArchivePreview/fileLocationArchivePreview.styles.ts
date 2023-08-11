0df1b8b097d275359c70a2f904cc6e258117776f
Fran McDade <frano-m@users.noreply.github.com>
2023-08-11 15:11:07 +1000

    chore: update data-explorer-ui to 34 (#901) (#3607)
    chore: update data-explorer-ui to latest (#901)
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- explorer/app/components/Detail/components/GeneratedMatricesTables/components/FileLocationArchivePreview/fileLocationArchivePreview.styles.ts
+++ explorer/app/components/Detail/components/GeneratedMatricesTables/components/FileLocationArchivePreview/fileLocationArchivePreview.styles.ts
@@ -1,7 +1,7 @@
-import { TextButtonPrimary } from "@clevercanary/data-explorer-ui/lib/components/common/Button/button.styles";
+import { ButtonTextPrimary } from "@clevercanary/data-explorer-ui/lib/components/common/Button/components/ButtonTextPrimary/buttonTextPrimary";
 import styled from "@emotion/styled";
 
-export const FileLocationArchivePreviewButton = styled(TextButtonPrimary)`
+export const FileLocationArchivePreviewButton = styled(ButtonTextPrimary)`
   font-weight: 400;
   justify-self: flex-start;
 `;
