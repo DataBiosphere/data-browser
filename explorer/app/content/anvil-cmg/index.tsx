d88256c50b9b3a6ad7dd0ff2fe30941f48f50e10
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-08-22 17:22:10 +1000

    feat: add login nag when not logged in (#3624)

--- explorer/app/content/anvil-cmg/index.tsx
+++ explorer/app/content/anvil-cmg/index.tsx
@@ -3,6 +3,7 @@ export { Section } from "../../components/common/MDXMarkdown/components/Section/
 export { default as ExportToTerraStart } from "../common/exportToTerraStart.mdx";
 export { default as ExportToTerraSuccess } from "../common/exportToTerraSuccess.mdx";
 export { default as DataReleasePolicy } from "./dataReleasePolicy.mdx";
+export { default as LoginReminder } from "./loginReminder.mdx";
 export { default as LoginTermsOfService } from "./loginTermsOfService.mdx";
 export { default as LoginText } from "./loginText.mdx";
 export { default as LoginWarning } from "./loginWarning.mdx";
