8dd7ef4630aedd7753a2c0cdcfa34361f76d5adb
Fran McDade <frano-m@users.noreply.github.com>
2023-09-19 02:58:06 +1000

    feat: implement bottom half of Terra account nag indicating the NIH account (#3658) (#3664)
    * feat: implement bottom half of Terra account nag indicating the NIH account (#3658)
    
    * chore: updated clevercanary/data-explorer-ui to v0.45.0 (#3658)
    
    ---------
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- explorer/site-config/anvil-cmg/cc-dev/authentication/authentication.ts
+++ explorer/site-config/anvil-cmg/cc-dev/authentication/authentication.ts
@@ -8,10 +8,14 @@ export const authenticationConfig: AuthenticationConfig = {
     googleProfileEndpoint: "https://www.googleapis.com/oauth2/v3/userinfo",
     scope:
       "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid",
+  },
+  termsOfService: MDX.RenderComponent({ Component: MDX.LoginTermsOfService }),
+  terraAuthConfig: {
+    terraNIHProfileEndpoint:
+      "https://firecloud-orchestration.dsde-dev.broadinstitute.org/api/nih/status",
     terraProfileEndpoint:
       "https://sam.dsde-dev.broadinstitute.org/register/user/v1",
   },
-  termsOfService: MDX.RenderComponent({ Component: MDX.LoginTermsOfService }),
   text: MDX.RenderComponent({ Component: MDX.LoginText }),
   title: "Sign in to your account",
   warning: MDX.RenderComponent({ Component: MDX.LoginWarning }),
