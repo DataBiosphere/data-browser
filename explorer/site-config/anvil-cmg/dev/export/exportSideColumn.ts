04cd0dd4325fa932a9102c52f60a3784eac23fe5
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-09-20 14:08:15 +1000

    feat: show show warning about missing files for datasets with access required on export (#3625)

---
+++ explorer/site-config/anvil-cmg/dev/export/exportSideColumn.ts
@@ -0,0 +1,38 @@
+import { ComponentConfig } from "../../../../../../data-explorer/packages/data-explorer-ui/src/config/entities";
+import * as C from "../../../../app/components";
+import * as MDX from "../../../../app/content/anvil-cmg";
+import * as V from "../../../../app/viewModelBuilders/azul/anvil-cmg/common/viewModelBuilders";
+
+export const sideColumn = [
+  {
+    children: [
+      {
+        children: [
+          {
+            component: C.ExportCurrentQuery,
+            viewBuilder: V.buildExportCurrentQuery,
+          } as ComponentConfig<typeof C.ExportCurrentQuery>,
+          {
+            component: C.ExportSelectedDataSummary,
+            viewBuilder: V.buildExportSelectedDataSummary,
+          } as ComponentConfig<typeof C.ExportSelectedDataSummary>,
+        ],
+        component: C.ExportSummary,
+      } as ComponentConfig<typeof C.ExportSummary>,
+      {
+        children: [
+          {
+            children: [
+              {
+                component: MDX.DataReleasePolicy,
+              } as ComponentConfig<typeof MDX.DataReleasePolicy>,
+            ],
+            component: MDX.Section,
+          } as ComponentConfig<typeof MDX.Section>,
+        ],
+        component: C.FluidPaper,
+      } as ComponentConfig<typeof C.FluidPaper>,
+    ],
+    component: C.BackPageContentSideColumn,
+  } as ComponentConfig<typeof C.BackPageContentSideColumn>,
+];
