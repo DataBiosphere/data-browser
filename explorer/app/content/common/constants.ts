048776147b8c7995147574dea0367b1104d7b2f5
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-11-06 16:14:29 +1000

    feat: add "bread crumbs" to tos and guides (#3740)

--- explorer/app/content/common/constants.ts
+++ explorer/app/content/common/constants.ts
@@ -1,7 +1,8 @@
+import { Breadcrumbs } from "../../components/common/Content/components/Breadcrumbs/breadcrumbs.styles";
 import { Figure } from "../../components/common/Figure/figure";
 
 export const CONTENT_FOLDER_NAME = "content";
 
-export const MDX_COMPONENTS = { Figure };
+export const MDX_COMPONENTS = { Breadcrumbs, Figure };
 
 export const MDX_SCOPE = {};
