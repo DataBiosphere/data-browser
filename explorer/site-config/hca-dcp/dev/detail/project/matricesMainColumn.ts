c68b660a1a26cbe33d08b4a9f331a81d7dc102dc
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-11-09 17:11:46 +1000

    feat: remove "got matrix questions " box (#3761)

--- explorer/site-config/hca-dcp/dev/detail/project/matricesMainColumn.ts
+++ explorer/site-config/hca-dcp/dev/detail/project/matricesMainColumn.ts
@@ -58,19 +58,6 @@ export const mainColumn: ComponentConfig[] = [
               {
                 children: [
                   {
-                    component: MDX.MatrixQuestionnaire,
-                  } as ComponentConfig<typeof MDX.MatrixQuestionnaire>,
-                ],
-                component: MDX.Section,
-              } as ComponentConfig<typeof MDX.Section>,
-            ],
-            component: C.FluidPaper,
-          } as ComponentConfig<typeof C.FluidPaper>,
-          {
-            children: [
-              {
-                children: [
-                  {
                     component: MDX.DataReleasePolicy,
                   } as ComponentConfig<typeof MDX.DataReleasePolicy>,
                 ],
