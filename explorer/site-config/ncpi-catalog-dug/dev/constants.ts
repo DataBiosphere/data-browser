dc93bbadee175be90a2e094bb29eee50f516cf21
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-12-12 18:13:24 +1000

    feat: remove ncpi dug config and associated view builders (#3829)

--- explorer/site-config/ncpi-catalog-dug/dev/constants.ts
+++
@@ -1,3 +0,0 @@
-export const DUG_API_URL = "https://search.biodatacatalyst.renci.org/";
-export const DUG_API_PARAMS = { index: "variables_index" };
-export const DUG_API_PATH = "search-api/search_var";
