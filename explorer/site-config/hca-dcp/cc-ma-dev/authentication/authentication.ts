beb76bd956342987c9370af0dd36c9fd5f58b234
Fran McDade <frano-m@users.noreply.github.com>
2023-12-14 17:11:30 +1000

    feat: update managed access config (#3833) (#3834)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- explorer/site-config/hca-dcp/cc-ma-dev/authentication/authentication.ts
+++ explorer/site-config/hca-dcp/cc-ma-dev/authentication/authentication.ts
@@ -1,12 +1,10 @@
 import { AuthenticationConfig } from "@clevercanary/data-explorer-ui/lib/config/entities";
 import * as MDX from "../../../../app/components/common/MDXContent/hca-dcp";
 
-// TODO(cc) update authentication config.
-
 export const authenticationConfig: AuthenticationConfig = {
   googleGISAuthConfig: {
     clientId:
-      "561542988117-9e04fhfrc9su130eb2ggea7bdppolkjq.apps.googleusercontent.com", // TODO(cc) update clientId.
+      "713613812354-ckf448r07ahvh8o1hjfj38tp91j9gpq8.apps.googleusercontent.com",
     googleProfileEndpoint: "https://www.googleapis.com/oauth2/v3/userinfo",
     scope:
       "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid",
