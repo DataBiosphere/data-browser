80dc448159547b2348f87ee02d1eaabbc592ba73
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-12-21 18:42:46 +1000

    feat: consolidate managed access (#3839)

--- explorer/site-config/hca-dcp/cc-ma-dev/config.ts
+++ explorer/site-config/hca-dcp/cc-ma-dev/config.ts
@@ -12,22 +12,32 @@ const DATA_URL = "https://service.dev.singlecell.gi.ucsc.edu";
 const PORTAL_URL = "https://data.humancellatlas.dev.clevercanary.com";
 
 const config: SiteConfig = {
-  ...makeConfig(BROWSER_URL, PORTAL_URL, DATA_URL, CATALOG),
+  ...makeManagedAccessConfig(
+    makeConfig(BROWSER_URL, PORTAL_URL, DATA_URL, CATALOG)
+  ),
   authentication: authenticationConfig,
 };
 
-// Adding authentication to the header.
-const header = { ...config.layout.header };
-config.layout.header = { ...header, authenticationEnabled: true };
+/**
+ * Returns managed access config.
+ * @param config - Site config.
+ * @returns managed access config.
+ */
+export function makeManagedAccessConfig(config: SiteConfig): SiteConfig {
+  // Adding authentication to the header.
+  const header = { ...config.layout.header };
+  config.layout.header = { ...header, authenticationEnabled: true };
 
-// Update entities.
-const entities = [...config.entities];
-config.entities = getMAEntitiesConfig(entities);
+  // Update entities.
+  const entities = [...config.entities];
+  config.entities = getMAEntitiesConfig(entities);
 
-// Update export.
-if (config.export) {
-  const exportConfig = { ...config.export };
-  config.export = getMAExportConfig(exportConfig);
+  // Update export.
+  if (config.export) {
+    const exportConfig = { ...config.export };
+    config.export = getMAExportConfig(exportConfig);
+  }
+  return config;
 }
 
 export default config;
--- explorer/site-config/hca-dcp/ma-prod/config.ts
+++ explorer/site-config/hca-dcp/ma-prod/config.ts
@@ -1,6 +1,6 @@
 import { SiteConfig } from "@clevercanary/data-explorer-ui/lib/config/entities";
+import { makeManagedAccessConfig } from "../cc-ma-dev/config";
 import { makeConfig } from "../dev/config";
-import maDevConfig from "../ma-dev/config";
 import { authenticationConfig } from "./authentication/authentication";
 
 // Template constants
@@ -9,21 +9,9 @@ const DATA_URL = "https://service.azul.data.humancellatlas.org";
 const PORTAL_URL = "https://data.humancellatlas.org";
 
 const config: SiteConfig = {
-  ...makeConfig(BROWSER_URL, PORTAL_URL, DATA_URL),
+  ...makeManagedAccessConfig(makeConfig(BROWSER_URL, PORTAL_URL, DATA_URL)),
   authentication: authenticationConfig,
 };
-
-// Adding authentication to the header.
-const header = { ...config.layout.header };
-config.layout.header = { ...header, authenticationEnabled: true };
-
-// Update entities.
-config.entities = [...maDevConfig.entities];
-
-// Update export.
-if (config.export) {
-  config.export = maDevConfig.export ? { ...maDevConfig.export } : undefined;
-}
 
 // Update gtmAuth for the prod environment lookup.
 if (config.analytics) {
