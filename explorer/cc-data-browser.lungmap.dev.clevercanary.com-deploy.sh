2d48248349f488d9b4a43743dea1cd468da40731
David Rogers <dave@clevercanary.com>
2024-01-29 20:10:40 -0800

    chore: 3872 lungmap dev build script (#3872) (#3877)

---
+++ explorer/cc-data-browser.lungmap.dev.clevercanary.com-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Set the script to exit immediately on error
+set -e
+
+echo \"Deleting ./out/\"
+rm -rf ./out
+
+n 16.15.1
+npm ci
+
+# Build
+npm run build-dev:lungmap
+
+export BUCKET=s3://6cc-data-browser.lungmap.dev/
+export SRCDIR=out/
+
+aws s3 sync  $SRCDIR $BUCKET --delete --profile excira
+aws cloudfront create-invalidation --distribution-id EBLFR0FQ3KCAQ --paths "/*" --profile excira
