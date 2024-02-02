de6895c37ff781d84e7947cf3360c2c4e45415bf
Fran McDade <frano-m@users.noreply.github.com>
2024-02-02 13:49:57 +1000

    feat: update lungmap config to a function (#3886) (#3887)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- explorer/app/content/common/contentPages.ts
+++ explorer/app/content/common/contentPages.ts
@@ -4,14 +4,23 @@ import { GetStaticPropsContext, GetStaticPropsResult } from "next";
 import { serialize } from "next-mdx-remote/serialize";
 import { MDX_SCOPE } from "./constants";
 import { ContentProps } from "./entities";
-import { getContentPathname, getMarkdownPathname, getSlug } from "./utils";
+import {
+  getContentPathname,
+  getMarkdownPathname,
+  getSlug,
+  isContentPathnameExists,
+} from "./utils";
 
 export async function getContentStaticProps(
   context: GetStaticPropsContext
 ): Promise<GetStaticPropsResult<ContentProps>> {
   const slug = getSlug(context);
   const contentPathname = getContentPathname();
-  if (!slug || !contentPathname) {
+  if (
+    !slug ||
+    !contentPathname ||
+    !isContentPathnameExists(contentPathname, slug)
+  ) {
     return {
       props: {
         frontmatter: null,
@@ -20,10 +29,8 @@ export async function getContentStaticProps(
       },
     };
   }
-  const markdownWithMeta = fs.readFileSync(
-    getMarkdownPathname(contentPathname, slug),
-    "utf-8"
-  );
+  const markdownPathname = getMarkdownPathname(contentPathname, slug);
+  const markdownWithMeta = fs.readFileSync(markdownPathname, "utf-8");
   const { content, data: frontmatter } = matter(markdownWithMeta);
   const mdxSource = await serialize(content, {
     mdxOptions: {
