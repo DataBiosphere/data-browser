0ae136fabb23eacf0c3dbbe0badf2689928572d1
EM <esthermcdade@EMs-MacBook-Pro.local>
2024-02-02 14:40:25 +1000

    feat: update guide images with border (#3888)

--- explorer/app/components/common/Figure/figure.styles.ts
+++ explorer/app/components/common/Figure/figure.styles.ts
@@ -1,3 +1,4 @@
+import { smokeMain } from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/colors";
 import { textBody400 } from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/fonts";
 import styled from "@emotion/styled";
 
@@ -5,6 +6,8 @@ export const Figure = styled.figure`
   margin: 32px 0;
 
   img {
+    border: 1px solid ${smokeMain};
+    border-radius: 6px;
     margin: 0 auto;
     max-width: 100%;
   }
