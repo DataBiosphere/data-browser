3bb9020f91dd5028b59e4cbe85ec8ee828504eff
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-04-03 16:48:33 +1000

    feat: update ma-prod catalog to "pilot" (#3964)

--- explorer/site-config/hca-dcp/ma-prod/config.ts
+++ explorer/site-config/hca-dcp/ma-prod/config.ts
@@ -5,11 +5,14 @@ import { getAuthenticationConfig } from "./authentication/authentication";
 
 // Template constants
 const BROWSER_URL = "https://ma-pilot.explore.data.humancellatlas.org";
+const CATALOG = "pilot";
 const DATA_URL = "https://service.azul.data.humancellatlas.org";
 const PORTAL_URL = "https://data.humancellatlas.org";
 
 const config: SiteConfig = {
-  ...makeManagedAccessConfig(makeConfig(BROWSER_URL, PORTAL_URL, DATA_URL)),
+  ...makeManagedAccessConfig(
+    makeConfig(BROWSER_URL, PORTAL_URL, DATA_URL, CATALOG)
+  ),
 };
 
 // Update gtmAuth for the prod environment lookup.
