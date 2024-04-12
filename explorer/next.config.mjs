de36508d3348a87134f310d12760954996c91cf9
Fran McDade <frano-m@users.noreply.github.com>
2024-04-12 14:43:30 +1000

    fix: remove need for unsafe-eval in csp (#3961) (#3980)
    * fix: remove need for unsafe-eval in csp (#3961)
    
    * fix: revert anvil cmg dev config (#3961)
    
    ---------
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- explorer/next.config.mjs
+++ explorer/next.config.mjs
@@ -1,95 +1,93 @@
-import withMDX from "@next/mdx";
+import nextMDX from "@next/mdx";
 import withPlugins from "next-compose-plugins";
 import path from "path";
 
-export default withPlugins([withMDX], {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  images: {
-    unoptimized: true,
-  },
-  output: "export",
-  reactStrictMode: true,
-  staticPageGenerationTimeout: 120,
-  webpack: (config) => {
-    config.module = {
-      ...config.module,
-      rules: [
-        ...config.module.rules,
-        {
-          test: /\.mdx?$/,
-          use: [
-            {
-              loader: "@mdx-js/loader",
-            },
-          ],
-        },
-      ],
-    };
-    // Add the alias for the peer dependency
-    config.resolve.alias["@emotion/react"] = path.resolve(
-      process.cwd(),
-      "node_modules/@emotion/react"
-    );
-    config.resolve.alias["@emotion/styled"] = path.resolve(
-      process.cwd(),
-      "node_modules/@emotion/styled"
-    );
-    config.resolve.alias["@mui/icons-material"] = path.resolve(
-      process.cwd(),
-      "node_modules/@mui/icons-material"
-    );
-    config.resolve.alias["@mui/material"] = path.resolve(
-      process.cwd(),
-      "node_modules/@mui/material"
-    );
-    config.resolve.alias["@tanstack/react-table"] = path.resolve(
-      process.cwd(),
-      "node_modules/@tanstack/react-table"
-    );
-    config.resolve.alias["axios"] = path.resolve(
-      process.cwd(),
-      "node_modules/axios"
-    );
-    config.resolve.alias["react-dropzone"] = path.resolve(
-      process.cwd(),
-      "node_modules/react-dropzone"
-    );
-    config.resolve.alias["isomorphic-dompurify"] = path.resolve(
-      process.cwd(),
-      "node_modules/isomorphic-dompurify"
-    );
-    config.resolve.alias["match-sorter"] = path.resolve(
-      process.cwd(),
-      "node_modules/match-sorter"
-    );
-    config.resolve.alias["next"] = path.resolve(
-      process.cwd(),
-      "node_modules/next"
-    );
-    config.resolve.alias["react"] = path.resolve(
-      process.cwd(),
-      "node_modules/react"
-    );
-    config.resolve.alias["react-dom"] = path.resolve(
-      process.cwd(),
-      "node_modules/react-dom"
-    );
-    config.resolve.alias["react-gtm-module"] = path.resolve(
-      process.cwd(),
-      "node_modules/react-gtm-module"
-    );
-    config.resolve.alias["react-window"] = path.resolve(
-      process.cwd(),
-      "node_modules/react-window"
-    );
-    config.resolve.alias["uuid"] = path.resolve(
-      process.cwd(),
-      "node_modules/uuid"
-    );
-    config.resolve.alias["validate.js"] = path.resolve(
-      process.cwd(),
-      "node_modules/validate.js"
-    );
-    return config;
-  },
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
 });
+
+export default withPlugins(
+  [
+    withMDX,
+    {
+      pageExtensions: ["md", "mdx", "ts", "tsx"],
+    },
+  ],
+  {
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+    images: {
+      unoptimized: true,
+    },
+    output: "export",
+    reactStrictMode: true,
+    staticPageGenerationTimeout: 120,
+    webpack: (config) => {
+      // Add the alias for the peer dependency
+      config.resolve.alias["@emotion/react"] = path.resolve(
+        process.cwd(),
+        "node_modules/@emotion/react"
+      );
+      config.resolve.alias["@emotion/styled"] = path.resolve(
+        process.cwd(),
+        "node_modules/@emotion/styled"
+      );
+      config.resolve.alias["@mui/icons-material"] = path.resolve(
+        process.cwd(),
+        "node_modules/@mui/icons-material"
+      );
+      config.resolve.alias["@mui/material"] = path.resolve(
+        process.cwd(),
+        "node_modules/@mui/material"
+      );
+      config.resolve.alias["@tanstack/react-table"] = path.resolve(
+        process.cwd(),
+        "node_modules/@tanstack/react-table"
+      );
+      config.resolve.alias["axios"] = path.resolve(
+        process.cwd(),
+        "node_modules/axios"
+      );
+      config.resolve.alias["react-dropzone"] = path.resolve(
+        process.cwd(),
+        "node_modules/react-dropzone"
+      );
+      config.resolve.alias["isomorphic-dompurify"] = path.resolve(
+        process.cwd(),
+        "node_modules/isomorphic-dompurify"
+      );
+      config.resolve.alias["match-sorter"] = path.resolve(
+        process.cwd(),
+        "node_modules/match-sorter"
+      );
+      config.resolve.alias["next"] = path.resolve(
+        process.cwd(),
+        "node_modules/next"
+      );
+      config.resolve.alias["react"] = path.resolve(
+        process.cwd(),
+        "node_modules/react"
+      );
+      config.resolve.alias["react-dom"] = path.resolve(
+        process.cwd(),
+        "node_modules/react-dom"
+      );
+      config.resolve.alias["react-gtm-module"] = path.resolve(
+        process.cwd(),
+        "node_modules/react-gtm-module"
+      );
+      config.resolve.alias["react-window"] = path.resolve(
+        process.cwd(),
+        "node_modules/react-window"
+      );
+      config.resolve.alias["uuid"] = path.resolve(
+        process.cwd(),
+        "node_modules/uuid"
+      );
+      config.resolve.alias["validate.js"] = path.resolve(
+        process.cwd(),
+        "node_modules/validate.js"
+      );
+      return config;
+    },
+  }
+);
