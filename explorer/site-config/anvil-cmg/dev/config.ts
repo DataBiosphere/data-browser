0253f8505efbe776d6650098b3b4a671647e9997
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-04-12 14:36:26 +1000

    fix: revert anvil cmg dev config (#3961)

--- explorer/site-config/anvil-cmg/dev/config.ts
+++ explorer/site-config/anvil-cmg/dev/config.ts
@@ -178,7 +178,7 @@ export function makeConfig(
 }
 
 const config: SiteConfig = makeConfig(
-  "https://explore.anvilproject.dev.clevercanary.com",
+  "https://explore.anvil.gi.ucsc.edu",
   "https://anvilproject.dev.clevercanary.com"
 );
 
