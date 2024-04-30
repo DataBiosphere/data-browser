9865f5609a22b1c2a797bc4f9f31ed02ad658763
Jonah Paten <jpaten@ucla.edu>
2024-04-29 17:58:00 -0700

    feat: added github action for anvil-catalog (#3638) (#4017)
    * feat: added github action for anvil-catalog (#3638)
    
    * fix: call npm ci in explorer/files (#3638)
    
    * fix: fixed github action db build script (#3638)
    
    * fix: fixed github action db build script for real this time(#3638)
    
    * fix: shortened long timeouts on certain anvil-catalog tests (#3638)
    
    * test: changed the consortium category label to make tests fail (UNDO ME) (#3638)
    
    * fix: made the anvil catalog tests action actually run catalog tests (#3638)
    
    * test: reverted deliberate change to make tests fail (#3638)
    
    * fix: readded playwright-report gitkeep (#3638)

--- explorer/playwright_anvil-catalog.config.ts
+++ explorer/playwright_anvil-catalog.config.ts
@@ -3,7 +3,7 @@ import { devices } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   expect: {
-    timeout: 20 * 1000,
+    timeout: 15 * 1000,
   },
   outputDir: "playwright-report/",
   projects: [
@@ -27,9 +27,10 @@ const config: PlaywrightTestConfig = {
     screenshot: "only-on-failure",
   },
   webServer: {
-    command: "npm run dev:anvil-catalog",
+    command:
+      "npm run build-dev:anvil-catalog && mv ./out/ ./out_temp && mkdir ./out && mv ./out_temp ./out/data && npm start",
     reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
+    timeout: 240 * 1000,
     url: "http://localhost:3000/data",
   },
 };
