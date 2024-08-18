2aca06a0a5b076dccdba6a44aaeca1b970d8dc0b
Jonah Paten <jpaten@ucla.edu>
2024-08-18 15:04:44 -0700

    chore: added system status check to anvil-cmg tests (#4108) (#4115)
    * chore: added system status check to anvil-cmg tests (#4108)
    
    * chore: added second system status check after tests run to ensure reliability (#4108)
    
    * chore: check that indexing checks fail appropriately (#4108)
    
    * chore: undo test failure check (#4108)

---
+++ explorer/e2e/checkIsIndexing.ts
@@ -0,0 +1,32 @@
+/**
+ * Check the system status, including whether the backend is currently indexing
+ * @param systemStatusUrl - a url to the system status api
+ */
+export async function checkIsIndexing(systemStatusUrl: string): Promise<void> {
+  const systemStatusResponse = await fetch(systemStatusUrl);
+  if (systemStatusResponse.status != 200) {
+    console.log(
+      "ERROR: The System Status API is currently unavailable, or an incorrect url was passed. Please rerun tests when this is resolved."
+    );
+    process.exit(1);
+  }
+  const systemStatusJson = await systemStatusResponse.json();
+  const isUp = systemStatusJson.up && systemStatusJson.progress.up;
+  const isIndexing =
+    systemStatusJson.progress.unindexed_bundles > 0 ||
+    systemStatusJson.progress.unindexed_documents > 0;
+  if (!isUp) {
+    console.log(
+      "There is an issue with the backend server. Please rerun tests once this issue has been resolved. If tests have already been run, please ignore the results and try again later."
+    );
+    process.exit(1);
+  } else if (isIndexing) {
+    console.log(
+      "ERROR: The database is currently indexing, which means that tests cannot run reliably. Please rerun tests later once indexing has stopped. If tests have already been run, please ignore the results and try again after indexing has stopped."
+    );
+    process.exit(1);
+  } else {
+    console.log("The System Status is currently good!");
+    process.exit(0);
+  }
+}
