0fbb25e7eea547108b31625c0bdc7fa0c7b16cc4
Hannes Schmidt <hannes@ucsc.edu>
2024-09-09 21:39:57 -0700

    Reuse MA pilot site for `dev`

--- explorer/site-config/hca-dcp/ma-dev/config.ts
+++ explorer/site-config/hca-dcp/ma-dev/config.ts
@@ -3,10 +3,10 @@ import { makeManagedAccessConfig } from "../cc-ma-dev/config";
 import { makeConfig } from "../dev/config";
 
 // Template constants
-const BROWSER_URL = "https://ma-pilot.explore.data.dev.singlecell.gi.ucsc.edu";
+const BROWSER_URL = "https://explore.dev.singlecell.gi.ucsc.edu";
 const CATALOG = "dcp3";
 const DATA_URL = "https://service.dev.singlecell.gi.ucsc.edu";
-const PORTAL_URL = "https://data.dev.singlecell.gi.ucsc.edu";
+const PORTAL_URL = "https://dev.singlecell.gi.ucsc.edu";
 
 const config: SiteConfig = {
   ...makeManagedAccessConfig(
