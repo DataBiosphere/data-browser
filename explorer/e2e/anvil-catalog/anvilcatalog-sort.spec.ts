9f18017f9a42b74b1dc256e0ed731ffd9932687b
jpaten <jonah@clevercanary.com>
2024-09-11 16:20:38 -0700

    test: removed redundant TODO (#4078)

--- explorer/e2e/anvil-catalog/anvilcatalog-sort.spec.ts
+++ explorer/e2e/anvil-catalog/anvilcatalog-sort.spec.ts
@@ -1,7 +1,6 @@
 import { test } from "@playwright/test";
 import { testSortCatalog } from "../testFunctions";
 import { ANVIL_CATALOG_TABS } from "./anvilcatalog-tabs";
-//TODO: handle boolean return value and fail if it's false for each test
 test("On the Consortia tab, expect clicking the column header (the sort button) to keep the first element of the column visible", async ({
   page,
 }) => {
