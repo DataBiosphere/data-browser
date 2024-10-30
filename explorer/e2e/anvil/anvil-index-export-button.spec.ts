ea3ac5e69860bfee64848a948b0218b3e49f51a9
jpaten <jonah@clevercanary.com>
2024-10-30 14:14:30 -0700

    test: added readme, cleanup on test descriptions (#4117)

--- explorer/e2e/anvil/anvil-index-export-button.spec.ts
+++ explorer/e2e/anvil/anvil-index-export-button.spec.ts
@@ -5,13 +5,13 @@ import {
 } from "../testFunctions";
 import { ANVIL_TABS } from "./anvil-tabs";
 
-test("Check that the export button on the index page functions as expected, on the Files tab", async ({
+test("Smoke test File Manifest Request index export workflow on the Diles tab", async ({
   page,
 }) => {
   await testIndexExportWorkflow(page, ANVIL_TABS.FILES);
 });
 
-test("Check that figures in the details tab on the index export page matches figures on the index page, on the BioSamples tab", async ({
+test("Check that figures in the details tab on the index export page matches figures on the index page on the BioSamples tab", async ({
   page,
 }) => {
   await testIndexExportDetails(page, ANVIL_TABS.BIOSAMPLES);
