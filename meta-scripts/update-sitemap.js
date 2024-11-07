15f1de14007d6d7821b749b77f3fc9d423af98ab
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-11-07 15:27:57 -0800

    style: run prettier on meta-scripts (#4230)

--- meta-scripts/update-sitemap.js
+++ meta-scripts/update-sitemap.js
@@ -1,10 +1,8 @@
-
-
-import { promisify } from "util";
 import fs from "fs";
 import got from "got";
 import { SitemapStream } from "sitemap";
 import { pipeline as callbackPipeline } from "stream";
+import { promisify } from "util";
 
 const pipeline = promisify(callbackPipeline);
 
@@ -14,8 +12,8 @@ const sitemapOutPath = "../dist/explore/projects-sitemap.xml";
 const scriptArgs = {};
 
 process.argv.slice(2).forEach((arg) => {
-    const [key, value] = arg.split("=");
-    scriptArgs[key] = value;
+  const [key, value] = arg.split("=");
+  scriptArgs[key] = value;
 });
 
 var hcaApiUrl;
@@ -23,43 +21,50 @@ var hcaApiUrl;
 // TODO: add stuff for the "env" argument
 
 if (!scriptArgs.catalog) {
-	console.log("Missing/invalid arguments");
-	console.log("Usage: npm run-script update-sitemap -- catalog=<catalog> env={dev|prod}");
-} else (async function() {
-	hcaApiUrl = "https://service.azul.data.humancellatlas.org/index/projects?catalog=" + scriptArgs.catalog + "&size=" + pageSize;
-	
-	const stream = new SitemapStream({ hostname: "https://data.humancellatlas.org/" });
-	const lastmod = new Date().toISOString();
-	
-	for (let entry of await getSearchResults(hcaApiUrl)) {
-		for (let project of entry.projects) {
-			stream.write({
-				url: "/explore/projects/" + project.projectId,
-				lastmod,
-				changefreq: "monthly"
-			});
-		}
-	}
-	
-	const pl = pipeline(stream, fs.createWriteStream(sitemapOutPath));
-	stream.end();
-	await pl;
-	
-	console.log("Done");
-})();
+  console.log("Missing/invalid arguments");
+  console.log(
+    "Usage: npm run-script update-sitemap -- catalog=<catalog> env={dev|prod}"
+  );
+} else
+  (async function () {
+    hcaApiUrl =
+      "https://service.azul.data.humancellatlas.org/index/projects?catalog=" +
+      scriptArgs.catalog +
+      "&size=" +
+      pageSize;
 
+    const stream = new SitemapStream({
+      hostname: "https://data.humancellatlas.org/",
+    });
+    const lastmod = new Date().toISOString();
 
-async function getSearchResults(url) {
-    // make a request using the url, paginate through the results, and return a combined results array
-
-    const allHits = [];
-
-    while (url) {
-        const { hits, pagination } = await got(url).json();
-        allHits.push(...hits);
-        url = pagination.next;
+    for (let entry of await getSearchResults(hcaApiUrl)) {
+      for (let project of entry.projects) {
+        stream.write({
+          url: "/explore/projects/" + project.projectId,
+          lastmod,
+          changefreq: "monthly",
+        });
+      }
     }
 
-    return allHits;
-}
+    const pl = pipeline(stream, fs.createWriteStream(sitemapOutPath));
+    stream.end();
+    await pl;
 
+    console.log("Done");
+  })();
+
+async function getSearchResults(url) {
+  // make a request using the url, paginate through the results, and return a combined results array
+
+  const allHits = [];
+
+  while (url) {
+    const { hits, pagination } = await got(url).json();
+    allHits.push(...hits);
+    url = pagination.next;
+  }
+
+  return allHits;
+}
