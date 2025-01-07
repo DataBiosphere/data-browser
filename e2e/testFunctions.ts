fb2cfd629fce4270f81c2e4623a6a62e377d0490
jpaten <jonah@clevercanary.com>
2025-01-07 14:21:35 -0800

    chore: set a viewport size for anvil cmg tests (#4329)

--- e2e/testFunctions.ts
+++ e2e/testFunctions.ts
@@ -235,7 +235,6 @@ export async function testSortAzul(
       await expect(sortIconLocator).not.toHaveCSS("opacity", "0");
       // Save the selected sort icon locator
       lastElementSortIconLocator = sortIconLocator;
-      await expect(false);
     }
   }
   return true;
--- playwright_anvil-catalog.config.ts
+++ playwright_anvil-catalog.config.ts
@@ -26,6 +26,7 @@ const config: PlaywrightTestConfig = {
   use: {
     baseURL: "http://localhost:3000/",
     screenshot: "only-on-failure",
+    viewport: { height: 1080, width: 1960 },
   },
   webServer: {
     command:
