ed5148d8f5dd4d9552e903ec737282f62c59e46f
jpaten <jonah@clevercanary.com>
2025-01-21 17:15:53 -0800

    chore: bumped ga package version (#4336)

--- analytics/analytics_package/setup.py
+++ analytics/analytics_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="analytics",
-	version="3.2.0",
+	version="3.3.0",
 	packages=["analytics"],
 	install_requires=["matplotlib", "pandas", "numpy", "google-auth-oauthlib", "google-api-python-client", "gspread", "gspread-formatting"],
 )
