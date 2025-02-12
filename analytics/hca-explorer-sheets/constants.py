fbe7255778fa1f1854a8aaea6d8b273af0a65262
jpaten <jonah@clevercanary.com>
2025-02-12 09:39:59 -0800

    feat: added missing elements to hca dx analytics sheets (#4371)

--- analytics/hca-explorer-sheets/constants.py
+++ analytics/hca-explorer-sheets/constants.py
@@ -1,20 +1,20 @@
 # CHANGE THESE VALUES TO GENERATE NEW REPORTS
 # The start and end dates of the current month (yyyy-mm-dd)
-START_DATE_CURRENT = "2024-12-01"
-END_DATE_CURRENT = "2024-12-31"
+START_DATE_CURRENT = "2025-01-01"
+END_DATE_CURRENT = "2025-01-31"
 # The start and end dates of the prior months
-START_DATE_PRIOR = "2024-11-01"
-END_DATE_PRIOR = "2024-11-30"
+START_DATE_PRIOR = "2024-12-01"
+END_DATE_PRIOR = "2024-12-31"
 # The name of the folder in which to save the report
-PARENT_FOLDER_NAME = "December 2024 (demos)"
+PARENT_FOLDER_NAME = "January 2025"
 
 # The name of the spreadsheet with the report
 SHEET_NAME = "HCA Explorer"
 
-HCA_PORTAL_ID = "361323030"
+HCA_ID = "361323030"
 # Filter to exclude the Data Explorer
-HCA_BROWSER_EXCLUDE_FILTER = {"filter": {"fieldName": "hostName", "stringFilter": {"matchType": "EXACT", "value": "explore.data.humancellatlas.org"}}}
+HCA_BROWSER_ONLY_FILTER = {"filter": {"fieldName": "hostName", "stringFilter": {"matchType": "EXACT", "value": "explore.data.humancellatlas.org"}}}
 SECRET_NAME = "HCA_ANALYTICS_REPORTING_CLIENT_SECRET_PATH"
-ANALYTICS_START = "2021-01-01"
+ANALYTICS_START = "2024-02-01"
 
 OAUTH_PORT = 8082
