273dcf5fbe4a37473b939160eaf36e4271b80362
jpaten <jonah@clevercanary.com>
2025-02-13 17:35:34 -0800

    feat: analytics sheets for anvil explorer (#4373)

---
+++ analytics/anvil-explorer-sheets/constants.py
@@ -0,0 +1,18 @@
+# CHANGE THESE VALUES TO GENERATE NEW REPORTS
+# The start and end dates of the current month (yyyy-mm-dd)
+START_DATE_CURRENT = "2025-01-01"
+END_DATE_CURRENT = "2025-01-31"
+# The start and end dates of the prior months
+START_DATE_PRIOR = "2024-12-01"
+END_DATE_PRIOR = "2024-12-31"
+# The name of the folder in which to save the report
+PARENT_FOLDER_NAME = "January 2025"
+
+# The name of the spreadsheet with the report
+SHEET_NAME = "AnVIL Explorer"
+ANVIL_EXPLORER_ID = "383267328"
+SECRET_NAME = 'ANVIL_ANALYTICS_REPORTING_CLIENT_SECRET_PATH'
+GA_PROPERTY_PORTAL = "368678391" # AnVIL Explorer - GA4
+ANALYTICS_START = "2024-01-01"
+
+OAUTH_PORT = 8082
