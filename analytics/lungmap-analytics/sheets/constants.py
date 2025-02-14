018933beca9f4a5bbf9ce606acf756922e6772c8
jpaten <jonah@clevercanary.com>
2025-02-13 18:01:01 -0800

    feat: added lungmap analytics reports (#4374)

---
+++ analytics/lungmap-analytics/sheets/constants.py
@@ -0,0 +1,19 @@
+# CHANGE THESE VALUES TO GENERATE NEW REPORTS
+# The start and end dates of the current month (yyyy-mm-dd)
+START_DATE_CURRENT = "2023-01-01"
+END_DATE_CURRENT = "2025-01-31"
+# The start and end dates of the prior months
+START_DATE_PRIOR = "2024-12-01"
+END_DATE_PRIOR = "2024-12-31"
+# The name of the folder in which to save the report
+PARENT_FOLDER_NAME = "January 2025"
+
+# The name of the spreadsheet with the report
+SHEET_NAME = "Lungmap Data Browser"
+HISTORIC_UA_DATA_PATH = "../users_over_time_history.json"
+LUNGMAP_ID = "362871218"
+SECRET_NAME = 'ANALYTICS_REPORTING_CLIENT_SECRET_PATH'
+GA_PROPERTY_PORTAL = "368678391" # Lungmap DX - GA4
+ANALYTICS_START = "2023-07-01"
+
+OAUTH_PORT = 8082
