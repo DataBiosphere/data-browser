539b54adf1222268968ef7a4989605220410984f
MillenniumFalconMechanic <mim@clevercanary.com>
2025-02-21 16:48:38 -0800

    feat: added check login required for hca export/download #4292

--- app/components/Detail/components/GeneratedMatricesTables/components/FileLocationCopy/fileLocationCopy.tsx
+++ app/components/Detail/components/GeneratedMatricesTables/components/FileLocationCopy/fileLocationCopy.tsx
@@ -1,6 +1,7 @@
 import { ButtonGroupButton } from "@databiosphere/findable-ui/lib/components/common/ButtonGroup/components/ButtonGroupButton/buttonGroupButton";
 import { ContentCopyIconSmall } from "@databiosphere/findable-ui/lib/components/common/CustomIcon/common/constants";
 import { useFileLocation } from "@databiosphere/findable-ui/lib/hooks/useFileLocation";
+import { useLoginGuard } from "@databiosphere/findable-ui/lib/providers/loginGuard/hook";
 import copy from "copy-to-clipboard";
 import { useEffect } from "react";
 import { ProjectMatrixView } from "../../../../../../viewModelBuilders/azul/hca-dcp/common/projectMatrixMapper/entities";
@@ -15,6 +16,9 @@ export const FileLocationCopy = ({
   const { url } = projectMatrixView;
   const { fileUrl, isLoading, isSuccess, run } = useFileLocation(url);
 
+  // Prompt user for login before download, if required.
+  const { requireLogin } = useLoginGuard();
+
   useEffect(() => {
     if (fileUrl && isSuccess) {
       copy(fileUrl);
@@ -26,7 +30,7 @@ export const FileLocationCopy = ({
       action="Copy project matrix"
       label={<ContentCopyIconSmall />}
       loading={isLoading}
-      onClick={run}
+      onClick={() => requireLogin(run)}
     />
   );
 };
