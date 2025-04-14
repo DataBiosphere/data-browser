bf065dad964f14d4e3186700ec35afec87494cea
MillenniumFalconMechanic <mim@clevercanary.com>
2025-04-14 14:53:49 -0700

    feat: added AnVIL data dictionary column defs #4551

--- app/config/utils.ts
+++ app/config/utils.ts
@@ -1,4 +1,11 @@
-import { SelectCategoryValue } from "@databiosphere/findable-ui/lib/common/entities";
+import {
+  Attribute,
+  AttributeValue,
+  DataDictionaryColumnDef,
+  SelectCategoryValue,
+} from "@databiosphere/findable-ui/lib/common/entities";
+import { CellContext, ColumnDef } from "@tanstack/react-table";
+import { BasicCell } from "@databiosphere/findable-ui/lib/components/DataDictionary/components/Table/components/BasicCell/basicCell";
 
 /**
  * Returns select category value with formatted label.
@@ -14,4 +21,30 @@ export function mapSelectCategoryValue(
       label: formatLabel(selectCategoryValue.label),
     };
   };
+}
+
+/**
+ * Returns an array of column defs, built from column def configurations, to
+ * use when displaying the data dictionary.
+ * @param columnDefConfigs - Array of column def configurations.
+ * @returns Array of column defs.
+ */
+export function buildColumnDefs(
+  columnDefConfigs: DataDictionaryColumnDef[]
+): ColumnDef<Attribute, AttributeValue>[] {
+  return columnDefConfigs.map((columnDefConfig) => {
+    const {
+      attributeDisplayName: header,
+      attributeSlotName: key,
+      width: { max, min },
+    } = columnDefConfig;
+    return {
+      accessorFn: (row) => row[key as keyof Attribute],
+      cell: (props: CellContext<Attribute, AttributeValue>) =>
+        BasicCell({ ...props }),
+      header: `${header}`,
+      id: key,
+      meta: { width: { max, min } },
+    };
+  });
 }
