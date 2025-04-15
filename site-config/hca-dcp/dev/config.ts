5199c303e1c52a2dbb33f1b024c8b4dc2b024f1a
Fran McDade <frano-m@users.noreply.github.com>
2025-04-15 10:38:52 +1000

    feat: implement chart view (#4455) (#4459)

--- site-config/hca-dcp/dev/config.ts
+++ site-config/hca-dcp/dev/config.ts
@@ -63,6 +63,7 @@ export function makeConfig(
       },
       url: `${dataUrl}/`,
     },
+    enableEntitiesView: true,
     entities: [projectsEntityConfig, samplesEntityConfig, filesEntityConfig],
     explorerTitle: "Explore Data",
     export: exportConfig,
