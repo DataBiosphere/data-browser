5e3ab4ad410870228554fc1aae1cde4a42992af1
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-12-15 12:08:20 +1000

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,13 @@ import withPlugins from "next-compose-plugins";
 const ESM_PACKAGES = [
   "ky",
   "@databiosphere/findable-ui",
-  "@mui/icons-material",
-  "@mui/material",
-  "@mui/system",
-  "@mui/types",
-  "@mui/utils",
-  "@emotion/react",
-  "@emotion/styled",
+  // "@mui/icons-material",
+  // "@mui/material",
+  // "@mui/system",
+  // "@mui/types",
+  // "@mui/utils",
+  // "@emotion/react",
+  // "@emotion/styled",
   "@observablehq/plot",
   "@tanstack/react-table",
   "@tanstack/react-virtual",
@@ -43,6 +43,12 @@ export default withPlugins(
     staticPageGenerationTimeout: 120,
     transpilePackages: [...ESM_PACKAGES],
     webpack: (config) => {
+      // config.resolve.alias["@emotion/react"] = r("@emotion/react");
+      // config.resolve.alias["@emotion/styled"] = r("@emotion/styled");
+      // config.resolve.alias["@mui/material"] = r("@mui/material");
+      // config.resolve.alias["@mui/system"] = r("@mui/system");
+      // config.resolve.alias["@mui/icons-material"] = r("@mui/icons-material");
+      // config.resolve.alias["@mui/utils"] = r("@mui/utils");
       config.resolve.alias["react"] = r("react");
       config.resolve.alias["react-dom"] = r("react-dom");
       return config;
