3b7fbfaa22ca1461a1b79f04393f082d96b7a872
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-05-13 13:39:41 +1000

    chore: switch og image to 512x512 brand icon, match anvil-portal summary card (#4797)

--- app/components/common/OgMeta/ogMeta.tsx
+++ app/components/common/OgMeta/ogMeta.tsx
@@ -57,17 +57,13 @@ export const OgMeta = ({
         property="og:description"
       />
       <meta key="og:image" content={image} property="og:image" />
-      <meta key="og:image:height" content="630" property="og:image:height" />
-      <meta key="og:image:width" content="1200" property="og:image:width" />
+      <meta key="og:image:height" content="512" property="og:image:height" />
+      <meta key="og:image:width" content="512" property="og:image:width" />
       <meta key="og:site_name" content={appTitle} property="og:site_name" />
       <meta key="og:title" content={title} property="og:title" />
       <meta key="og:type" content="website" property="og:type" />
       <meta key="og:url" content={url} property="og:url" />
-      <meta
-        key="twitter:card"
-        content="summary_large_image"
-        name="twitter:card"
-      />
+      <meta key="twitter:card" content="summary" name="twitter:card" />
       <meta key="twitter:image" content={image} name="twitter:image" />
     </NextHead>
   );
