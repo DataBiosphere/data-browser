b02ba98f0ed52df2a287e9199afd26351cdeefa8
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-08-14 18:15:13 -0700

    fix: normalize `setup.py` indentation to four spaces (#4912)

--- analytics/analytics_package/setup.py
+++ analytics/analytics_package/setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-	name="analytics",
-	version="5.0.2",
-	packages=["analytics", "analytics.static_site"],
-	package_data={"analytics.static_site": ["template/*.html"]},
-	install_requires=[
-    "pandas",
-    "numpy",
-    "google-auth-oauthlib",
-    "google-api-python-client",
-    "requests",
-  ],
+    name="analytics",
+    version="5.0.2",
+    packages=["analytics", "analytics.static_site"],
+    package_data={"analytics.static_site": ["template/*.html"]},
+    install_requires=[
+        "pandas",
+        "numpy",
+        "google-auth-oauthlib",
+        "google-api-python-client",
+        "requests",
+    ],
 )
